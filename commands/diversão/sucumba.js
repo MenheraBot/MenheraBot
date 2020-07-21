167479c2f96d61e32549176886bb397041557281
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-07-21 19:01:03 -0300

    bug fixed

--- commands/diversão/sucumba.js
+++ commands/diversão/sucumba.js
@@ -11,5 +11,5 @@ module.exports = {
   if(!user) return message.reply("n/a");
   if(user.id == message.author.id) return message.reply("n/a");
 
-  message.channel.send(`SUCUMBA **${user}**\nVERME\nLIXO\nHORROROSO\nRUIM\nHORRÍVEL\nESCÓRIA\nBOSTA\nLIXOSO\nPERITO EM ENTREGAR GAME\n"COCOZENTO`);
+  message.channel.send(`SUCUMBA **${user}**\nVERME\nLIXO\nHORROROSO\nRUIM\nHORRÍVEL\nESCÓRIA\nBOSTA\nLIXOSO\nPERITO EM ENTREGAR GAME\nCOCOZENTO`);
 }};
