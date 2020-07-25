f2ed3a557e19f8ff48c19a9ac75e5cd707404072
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-07-25 00:29:23 -0300

    added resmungar command

---
+++ commands/diversão/resmungar.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "resmungar",
+  aliases: ["grumble", "grumbling", "resmungo", "humpf"],
+  cooldown: 2,
+  category: "diversão",
+  description: "Resmungue humpf",
+  usage: "m!resmungar",
+  run: async (client, message, args) => {
+  
+  let avatar = message.author.displayAvatarURL({ format: "png" });
+  
+  var list = [
+    "https://i.imgur.com/l1jwHGy.gif",
+    "https://i.imgur.com/4co1K8h.gif",
+    "https://i.imgur.com/XAcuQN9.gif",
+    "https://i.imgur.com/JeolGmS.gif",
+    "https://i.imgur.com/lGUJNbY.gif",
+    "https://i.imgur.com/V9XR3VN.gif"
+  ];
+
+  var rand = list[Math.floor(Math.random() * list.length)];
+  const avatar = message.author.displayAvatarURL();
+  
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Humpf")
+    .setColor("#000000")
+    .setDescription(`${message.author} está resmungando`)
+    .setThumbnail(avatar)
+    .setImage(rand)
+    .setFooter("Humpf 0.o")
+    .setAuthor(message.author.tag, avatar);
+
+   message.channel.send(embed);
+   
+}};
