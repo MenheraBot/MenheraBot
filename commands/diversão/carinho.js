046e87d494d0343b37ac58784a510e4f7226c337
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-07-25 00:21:11 -0300

    Links add

--- commands/diversão/carinho.js
+++ commands/diversão/carinho.js
@@ -15,7 +15,8 @@ module.exports = {
     "https://i.imgur.com/2k0MFIr.gif",
     "https://i.imgur.com/XjsEMiK.gif",
     "https://i.imgur.com/sLwoifL.gif",
-    "https://i.imgur.com/TPqMPka.gif"
+    "https://i.imgur.com/TPqMPka.gif",
+    "https://i.imgur.com/9CvzZTE.gif"
   ];
 
   var rand = list[Math.floor(Math.random() * list.length)];
