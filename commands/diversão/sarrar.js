36d6cbe2d8ea43067065ac93572c08cc14bf37f1
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-07-26 15:29:40 -0300

    Sarrar modificado

--- commands/diversão/sarrar.js
+++ commands/diversão/sarrar.js
@@ -9,45 +9,63 @@ module.exports = {
   usage: "m!sarrar [@menção]",
   run: async (client, message, args) => {
 
-  var listaSozinho = [
-        "https://media1.tenor.com/images/e0b093e5174a74518ffcbc0967d265eb/tenor.gif?itemid=17767202",
-        "https://i.imgur.com/XBcmgBR.png"
-  ];
+    var listaSozinho = [
+      "https://media1.tenor.com/images/e0b093e5174a74518ffcbc0967d265eb/tenor.gif?itemid=17767202",
+      "https://i.imgur.com/XBcmgBR.png"
+    ];
 
-  var randSozinho = listaSozinho[Math.floor(Math.random() * listaSozinho.length)];
-  let user = message.mentions.users.first();
-  
-  if (!user) {
-    const embed = new Discord.MessageEmbed()
-    .setTitle("Sarra comigo?")
-    .setColor("#000000")
-    .setDescription(`Alguém quer sarrar com ${message.author}?`)
-    .setImage(randSozinho)
-    .setThumbnail(message.author.displayAvatarURL())
-    .setFooter("Quer sarrar comigo?")
-    .setAuthor(message.author.tag, message.author.displayAvatarURL());
+    var randSozinho = listaSozinho[Math.floor(Math.random() * listaSozinho.length)];
+    let user = message.mentions.users.first();
 
-    return message.channel.send(embed);
+    if (!user) {
+      const embed = new Discord.MessageEmbed()
+        .setTitle("Sarra comigo?")
+        .setColor("#000000")
+        .setDescription(`Alguém quer sarrar com ${message.author}?\nReaja para sarrar`)
+        .setImage(randSozinho)
+        .setThumbnail(message.author.displayAvatarURL())
+        .setFooter("Quer sarrar comigo? 30 segundos para aceitar a sarrada")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL());
+
+      return message.channel.send(embed).then(msg => {
+
+        msg.react("✅").catch(err => message.channel.send("Ocorreu um erro ao adicionar uma reação, serasi eu tenho permissão para tal?"));
+        let filter = (reaction, usuario) => reaction.emoji.name === "✅" && usuario.id !== message.author.id && !usuario.bot;
+
+        let coletor = msg.createReactionCollector(filter, { max: 1, time: 30000 });
+
+        coletor.on("collect", (react, user) => {
+
+          sarrada(message, user);
+
+        });
+
+      });
+    } else return sarrada(message, message.mentions.users.first());
+
   }
+}
 
-  
+function sarrada(message, reactUser) {
+
+
   var lista = [
     "https://media1.tenor.com/images/209f3b2875d4834f82fec9531a60412e/tenor.gif?itemid=17770012"
-];
+  ];
 
-var rand = lista[Math.floor(Math.random() * lista.length)];
+  var rand = lista[Math.floor(Math.random() * lista.length)];
 
   let avatar = message.author.displayAvatarURL({ format: "png" });
 
   const Embed = new Discord.MessageEmbed()
 
-  .setTitle("Sarrar")
-  .setColor("#000000")
-  .setDescription(`${message.author} sarrou com ${user}`)
-  .setImage(rand)
-  .setThumbnail(avatar)
-  .setFooter("Nós somos os sarradores profissionais")
-  .setAuthor(message.author.tag, avatar);
+    .setTitle("Sarrar")
+    .setColor("#000000")
+    .setDescription(`${message.author} sarrou com ${reactUser}`)
+    .setImage(rand)
+    .setThumbnail(avatar)
+    .setFooter("Nós somos os sarradores profissionais")
+    .setAuthor(message.author.tag, avatar);
 
-   message.channel.send(Embed);
-  }}
+  message.channel.send(Embed);
+}
