c005c0120276ace395996b5baeb4132ba61e2983
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-08-13 11:56:26 -0300

    little fixes

--- commands/Dev/find.js
+++ commands/Dev/find.js
@@ -5,7 +5,7 @@ module.exports = {
     aliases: ["achar"],
     cooldown: 2,
     category: "Dev",
-    description: "Arquivo destinado para testes",
+    description: "Retorna um servidor ou um usuario",
     usage: "m!find <user|server> <id>",
     run: async (client, message, args) => {
         if(message.author.id !== '435228312214962204') return message.channel.send("Este comando é único de minha dona!")
