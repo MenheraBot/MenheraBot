78dfceeeaa4c7be14d776ae9470680b5d7539a40
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-08-13 10:35:34 -0300

    rename folder

---
+++ commands/Dev/act.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "act",
+  aliases: ["atividade", "activity"],
+  cooldown: 2,
+  category: "BotConfig",
+  description: "Altera a atividade do bot",
+  usage: "m!act [atividade]",
+
+  run: async (client, message, args) => {
+  if (message.author == client.users.cache.get('435228312214962204')) {
+    message.delete().catch(erro =>message.reply("Ocorreu um erro inesperado, perdão. \n Erro: " + erro));
+    client.user.setActivity(args.join(" "), { type: "WATCHING" });
+  } else return message.reply("Sem permissão");
+}};
---
+++ commands/Dev/test.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "test",
+    aliases: ["teste"],
+    cooldown: 2,
+    category: "BotConfig",
+    description: "Arquivo destinado para testes",
+    usage: "m!test [comando]",
+    run: async (client, message, args) => {
+    // let member = await client.users.fetch(args[0].replace(/[<@!>]/g, ""))
+    //return message.channel.send(`O membro é ${member}\nCujo id é ${member.id}`);
+    }}
