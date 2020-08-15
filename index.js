261fc46827107ee428643e8991822877cd9c37fe
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-08-15 00:59:16 -0300

    deleted comments

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.categories = fs.readdirSync("./commands/");
 
-//client.cooldowns = new Discord.Collection();
-
 ["command"].forEach(handler => {
   require(`./handler/${handler}`)(client);
 })
@@ -36,24 +34,6 @@ client.on("message", async message => {
   let command = client.commands.get(cmd);
   if(!command) command = client.commands.get(client.aliases.get(cmd));
 
-  /* const now = Date.now();
-  const timestamps = client.cooldowns.get(command.name);
-  const cooldownAmount = (command.cooldown || 3) * 1000;
-  
-  if (timestamps.has(message.author.id)) {
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-	if (now < expirationTime) {
-		const timeLeft = (expirationTime - now) / 1000;
-		return message.reply(`por favor, espere ${timeLeft.toFixed(1)} segundos antes de usar o comando \`${command.name}\``).then(m => {
-      if(m.deletable) m.delete({timeout: 3000})
-      if(message.deletable) message.delete({timeout: 3000})
-    });
-  }}
-  
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-  */
   if(command){
      command.run(client, message, args).catch(err => {
        console.log(err);
@@ -93,7 +73,6 @@ client.on("ready", () => {
 
 process.on('unhandledRejection', error =>	console.error('Unhandled promise rejection:', error));
 process.on('warning', e => console.warn(e.stack));
-
 
 //mudar para config.token para logar na menhrea
 //testToken = DevBot
