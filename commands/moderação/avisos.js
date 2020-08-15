d4531747e97f22bcee3c02613f16e56c9f22a0bd
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-08-15 04:58:49 -0300

    avisos command added

---
+++ commands/moderação/avisos.js
@@ -0,0 +1,58 @@
+const Discord = require("discord.js");
+
+const Warn = require("../../models/warn.js")
+
+module.exports = {
+  name: "avisos",
+  aliases: ["warns", "warnslist", "punishmentlist"],
+  cooldown: 2,
+  category: "moderação",
+  description: "Veja quantos warns uma pessoa tem",
+  usage: "m!avisos <usuario>",
+  run: async (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Você precisa da permissão `MANAGE_MESSAGES` para utilizar este comando");
+
+    const user = message.mentions.users.first() || client.users.cache.get(args[0]);
+    if(!user) return message.reply("Nenhum usuário encontrado");
+    if(user.bot) return message.reply("Bots são muito legais para receberem avisos");
+
+    //listas
+
+    var noWarn = [
+        "https://i.imgur.com/pwMKAPd.png",
+        "https://i.imgur.com/d8cgWvS.png",
+        "https://i.imgur.com/aVXTWSA.jpg",
+        "https://i.imgur.com/bUuehyU.jpg",
+        "https://i.imgur.com/4FfgL7h.png"
+    ];
+
+    var warned = [
+        "https://i.imgur.com/vS46DHp.png",
+        "https://i.imgur.com/ziNWVxo.jpg",
+        "https://i.imgur.com/ZVQh20v.jpg",
+        "https://i.imgur.com/oPTIn2Z.jpg",
+        "https://i.imgur.com/2kxswoS.png"
+    ];
+
+    let rand;
+
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`Avisos de ${user.tag}`)
+
+    Warn.find({userId: user.id, guildId: message.guild.id}).sort([['data', 'ascending']]).exec((err, db) => {
+        if(err) console.log(err);
+
+        if(!db || db.length < 1){
+            embed.setDescription(`${user} não possui avisos`);
+             rand = noWarn[Math.floor(Math.random() * noWarn.length)];
+        } else {
+            rand = warned[Math.floor(Math.random() * warned.length)];
+        }
+
+        for(i = 0; i < db.length; i++){
+            embed.addField(`Aviso #${i + 1}`, `**Avisado por:** ${client.users.cache.get(db[i].warnerId)}\n**Razão:** ${db[i].reason}\n**Data:** ${db[i].data}`);
+        }
+        embed.setImage(rand);
+        message.channel.send(embed);
+    })
+  }};
