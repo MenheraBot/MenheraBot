ecd43c812f3d0f2077be1d455d69ab51af1fcc4a
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-08-14 22:19:54 -0300

    Removed ID

--- commands/moderação/say.js
+++ commands/moderação/say.js
@@ -10,7 +10,7 @@ module.exports = {
   const sayMessage = args.join(" ");
   if(!sayMessage) return message.reply("Oq quer que eu fale?")
   message.delete().catch(O_o => {});
-  if (message.author.id == "435228312214962204" || message.member.hasPermission("MANAGE_MESSAGES")) {
+  if (message.member.hasPermission("MANAGE_MESSAGES")) {
    return message.channel.send(sayMessage);
   }
   return message.reply("FRACO... LHE FALTA PODER")
