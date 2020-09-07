dc6d6d71da76462b6740874b1f33ef57e746c0bf
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-09-07 15:04:14 -0300

    eval

--- commands/Dev/eval.js
+++ commands/Dev/eval.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 const database = require("../../models/user.js");
 const Warns = require("../../models/warn.js");
 
+const { inspect } = require('util')
+
 module.exports = {
   name: "eval",
   aliases: ["run", "execute"],
@@ -12,24 +14,25 @@ module.exports = {
   usage: "m!eval <comando>",
 
   run: async (client, message, args) => {
-    
-    if(message.author.id !== '435228312214962204') return message.reply("Este comando é exclusivo da minha Dona");
+  if(message.author.id !== '435228312214962204') return message.reply("Este comando é exclusivo da minha Dona");
 
-    try {
-        const code = args.join(" ");
-        let evaled = eval(code);
-   
-        if (typeof evaled !== "string")
-          evaled = require("util").inspect(evaled);
-   
-        message.channel.send(clean(evaled), {code:"xl"}).catch(err => message.channel.send(`Erro no retorno do código \`\`\`js\n${err}\`\`\``))
-      } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-      }
-}};
-function clean(text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
+  try {
+    const util = require("util")
+    let evaled = await eval(args.join(" "))
+    evaled = util.inspect(evaled, { depth: 1 })
+    evaled = evaled.replace(new RegExp(`${client.token}`, "g"), undefined)
+
+    if (evaled.length > 1800) evaled = `${evaled.slice(0, 1800)}...`
+    message.channel.send(evaled, { code: "js" })
+  } catch (err) {
+    const errorMessage = err.stack.length > 1800 ? `${err.stack.slice(0, 1800)}...` : err.stack
+    const embed = new Discord.MessageEmbed()
+    embed.setColor('#ff0000')
+    embed.setTitle(`Erro :()`)
+    embed.setDescription(`\`\`\`js\n${errorMessage}\`\`\``)
+
+    message.channel.send(embed)
   }
+
+
+}};
