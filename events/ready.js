3f2bf07c3f15b1a71a774d5ff7784cdca65d670b
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-09-21 22:05:47 -0300

    Back  menhera's activity

--- events/ready.js
+++ events/ready.js
@@ -10,16 +10,14 @@ module.exports = (client) => {
     dbl.on("error", console.error)
   }
 
- // client.user.setActivity("Fui reiniciada com sucesso uwu")
+ client.user.setActivity("Fui reiniciada com sucesso uwu")
     
   let status = [
     {name: "a moon ser perfeita", type: "WATCHING"},
     {name: "o meu servidor de suporte m!suporte", type: "LISTENING"},
     {name: "sabia que a moon é a salvação da minha dona? sem moon, menhera = inexistente m!moon", type: "PLAYING"},
     {name: "a vida é dificil, mas estamos aqui pra facilitá-la", type: "PLAYING"},
-    {name: "skilly é meu sonho de vida", type: "PLAYING"},
     {name: "já votou em mim hoje? m!votar", type: "PLAYING"},
-    {name: "Minha dona está apaixonadinha uwuwuwu", type: "PLAYING"},
     {name: "Caçe demônios com XANDÃO. m!caçar", type: "PLAYING"},
     {name: "Tem ideia de um comando interessante? Use m!sugerir", type: "PLAYING"},
     {name: "Pergunte para mim. m!8ball", type: "PLAYING"},
@@ -28,8 +26,8 @@ module.exports = (client) => {
     {name: "Duvidas? Entre em meu servidor de suporte m!suporte", type: "PLAYING"}
     ];
 
-  //setInterval(() => {
-  //  let randomStatus = status[Math.floor(Math.random() * status.length)]
-  //  client.user.setPresence({ activity: randomStatus })
-  //}, 1000 * 60);
+  setInterval(() => {
+    let randomStatus = status[Math.floor(Math.random() * status.length)]
+    client.user.setPresence({ activity: randomStatus })
+  }, 1000 * 60);
 }
