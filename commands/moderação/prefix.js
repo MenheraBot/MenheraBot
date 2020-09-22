c2bf2fe45aa41cb46b64b2be1944232ae521c1f6
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-09-21 21:34:56 -0300

    Fixed m!prefix

--- commands/moderação/prefix.js
+++ commands/moderação/prefix.js
@@ -11,10 +11,12 @@ module.exports = {
     if (!message.member.hasPermission("MANAGE_GUILD")) return message.reply("Você precisa da permissão de Gerenciar Servidor para utilizar este comando");
     
 		if (!args[0]) return message.reply("Qual é o prefixo que deseja colocar para este servidor?")
-		if (args[0].length > 3) return message.reply("Este prefixo é muito longo! Escolha um prefixo com no máximo 3 caracteres")
-		serverDb.prefix = args[0]
-		serverDb.save()
+        if (args[0].length > 3) return message.reply("Este prefixo é muito longo! Escolha um prefixo com no máximo 3 caracteres")
+        
+        const server = await serverDb.findOne({id: message.guild.id})
+		server.prefix = args[0]
+		server.save()
 
-		message.reply("✅ | Certo, meu prefixo neste servidor passa a ser " + serverDb.prefix);
+		message.reply("✅ | Certo, meu prefixo neste servidor passa a ser " + server.prefix);
 
   }};
