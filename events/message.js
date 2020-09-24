b5135ca7d2253f2c3ed8a68eeeb075bae25a70c3
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-09-24 11:02:06 -0300

    Ping System

--- events/message.js
+++ events/message.js
@@ -9,6 +9,9 @@ const cooldown = new Set();
 
 module.exports = async (client, message) => {
 
+  //ping system
+  if(message.author.id === "757295289630720031" && message.content === "PINGING...") return message.channel.send("Pong! That's my response to your call, master");
+
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
