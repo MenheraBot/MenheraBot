3f641d12fbe87b37cdd8943eeeaaba67a8555b35
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-09-26 14:14:25 -0300

    New Aliases

--- commands/economia/carteira.js
+++ commands/economia/carteira.js
@@ -2,7 +2,7 @@ const usuario = require("../../models/user.js");
 
 module.exports = {
     name: "carteira",
-    aliases: ["money", "banco", "bank"],
+    aliases: ["money", "banco", "bank", "inventario", "inventário", "inventory"],
     cooldown: 2,
     category: "economia",
     description: "Veja a carteira de alguém",
@@ -11,10 +11,11 @@ module.exports = {
 
         let pessoa = message.mentions.users.first() || client.users.cache.get(args[0]);
         if (!pessoa) pessoa = message.author;
-          
-        let user = await usuario.findOne({id: pessoa.id});
-        if(!user) return message.channel.send("❌ | Este usuário não está em minha database")
+
+        let user = await usuario.findOne({ id: pessoa.id });
+        if (!user) return message.channel.send("❌ | Este usuário não está em minha database")
 
         message.channel.send(`**${pessoa.tag}** possui **${user.estrelinhas}**⭐ e **${user.rolls}** 🔑\nSuas caças são:\n\n<:DEMON:758765044443381780> **${user.caçados}** demônios\n<:ANGEL:758765044204437535> **${user.anjos}** anjos\n<:SEMIGOD:758766732235374674> **${user.semideuses}** semideuses\n<:God:758474639570894899> **${user.deuses}** deuses.`)
- }}
+    }
+}
 
