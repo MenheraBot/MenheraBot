993857b8d44b5cca6006ae786d9ce57b5c003719
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-09-26 15:12:02 -0300

    Inventory

--- commands/economia/inventário.js
+++ commands/economia/inventário.js
@@ -1,8 +1,8 @@
 const usuario = require("../../models/user.js");
 
 module.exports = {
-    name: "carteira",
-    aliases: ["money", "banco", "bank", "inventario", "inventário", "inventory"],
+    name: "inventário",
+    aliases: ["inventario", "carteira", "inventory"],
     cooldown: 2,
     category: "economia",
     description: "Veja a carteira de alguém",
@@ -15,7 +15,7 @@ module.exports = {
         let user = await usuario.findOne({ id: pessoa.id });
         if (!user) return message.channel.send("❌ | Este usuário não está em minha database")
 
-        message.channel.send(`**${pessoa.tag}** possui **${user.estrelinhas}**⭐ e **${user.rolls}** 🔑\nSuas caças são:\n\n<:DEMON:758765044443381780> **${user.caçados}** demônios\n<:ANGEL:758765044204437535> **${user.anjos}** anjos\n<:SEMIGOD:758766732235374674> **${user.semideuses}** semideuses\n<:God:758474639570894899> **${user.deuses}** deuses.`)
+        message.channel.send(`**Inventário de ${pessoa.tag}**\n\n⭐ **${user.estrelinhas}** estrelinhas\n🔑 **${user.rolls}** rolls\n<:DEMON:758765044443381780> **${user.caçados}** demônios\n<:ANGEL:758765044204437535> **${user.anjos}** anjos\n<:SEMIGOD:758766732235374674> **${user.semideuses}** semideuses\n<:God:758474639570894899> **${user.deuses}** deuses.`)
     }
 }
 
