bbdafe0c014a4bf8c259a261bcd60bc8eef666d8
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-09-27 14:35:26 -0300

    Caçar in support server

--- commands/diversão/caçar.js
+++ commands/diversão/caçar.js
@@ -19,43 +19,72 @@ module.exports = {
 
     const validArgs = [
       {
-          opção: "demônio",
-          arguments: ["demonios", "demônios", "demons", "demonio", "demônio"]
+        opção: "demônio",
+        arguments: ["demonios", "demônios", "demons", "demonio", "demônio"]
       },
       {
-          opção: "anjos",
-          arguments: ["anjos", "anjo", "angels"]
+        opção: "anjos",
+        arguments: ["anjos", "anjo", "angels"]
       },
       {
-          opção: "semideuses",
-          arguments: ["semideuses", "semideus", "semi-deuses", "sd", "semi-deus"]
+        opção: "semideuses",
+        arguments: ["semideuses", "semideus", "semi-deuses", "sd", "semi-deus"]
       },
       {
-          opção: "deus",
-          arguments: ["deus", "deuses", "gods", "god"]
+        opção: "deus",
+        arguments: ["deus", "deuses", "gods", "god"]
       },
       {
-          opção: "ajuda",
-          arguments: ["ajudas", "help", "h", "ajuda"]
+        opção: "ajuda",
+        arguments: ["ajudas", "help", "h", "ajuda"]
       },
       {
         opção: "probabilidades",
         arguments: ["probabilidades", "probabilidade", "probability", "probabilities"]
       }
-  ];
+    ];
 
 
     if (!args[0]) return message.channel.send(`<:negacao:759603958317711371> | ${message.author}, você deve escolher entre caçar \`${validOptions.join("`, `")}\``)
     const selectedOption = validArgs.some(so => so.arguments.includes(args[0].toLowerCase()))
-      if (!selectedOption) return message.channel.send(`<:negacao:759603958317711371> | ${message.author}, você deve escolher entre caçar \`${validOptions.join("`, `")}\``)
-        const filtredOption = validArgs.filter(f => f.arguments.includes(args[0].toLowerCase()))
+    if (!selectedOption) return message.channel.send(`<:negacao:759603958317711371> | ${message.author}, você deve escolher entre caçar \`${validOptions.join("`, `")}\``)
+    const filtredOption = validArgs.filter(f => f.arguments.includes(args[0].toLowerCase()))
 
-        const option = filtredOption[0].opção
+    const option = filtredOption[0].opção
 
     if (!option) return message.channel.send(`<:negacao:759603958317711371> | ${message.author}, você deve escolher entre caçar \`${validOptions.join("`, `")}\``)
 
-    if (option === "ajuda") return message.channel.send("**COMO FUNCIONA O CAÇAR?**\nVocê pode caçar a cada uma hora, e você pode escolher entre 4 caças: `demonios`, `anjos`, `semideuses` e `deuses`\nQuanto mais pra direita, mais valioso, mas também, mais dificil de se caçar, e menor a chance de sucesso\n**Boas Caçadas**")
-    if (option === "probabilidades") return message.channel.send("**PROBABILIDADES:**\nÉ selecionado um número aleatório dentro dos próximos. Cada número é a quantidade de caças\n\nDemônios = `[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 4]`\nAnjos = `[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]`\nSemiDeuses = `[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];`\nDeuses = `[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]`")
+    //probabilidades normais
+    const probabilidadeDemonioBasica = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 4];
+    const probabilidadeAnjoBasica = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2];
+    const probabilidadeSDBasica = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const probabilidadeDeusesBasica = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+    //probabilidades do servidor de suporte
+    const probabilidadeDemonioServer = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 4];
+    const probabilidadeAnjoServer = [0, 0, 0, 1, 1, 1, 1, 2];
+    const probabilidadeSDServer = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const probabilidadeDeusesServer = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+
+    let probabilidadeDemonio;
+    let probabilidadeAnjo;
+    let probabilidadeSD;
+    let probabilidadeDeuses;
+
+    if (message.guild.id === '717061688460967988') {
+      probabilidadeDemonio = probabilidadeDemonioServer;
+      probabilidadeAnjo = probabilidadeAnjoServer;
+      probabilidadeSD = probabilidadeSDServer;
+      probabilidadeDeuses = probabilidadeDeusesServer;
+    } else {
+      probabilidadeDemonio = probabilidadeDemonioBasica;
+      probabilidadeAnjo = probabilidadeAnjoBasica;
+      probabilidadeSD = probabilidadeSDBasica;
+      probabilidadeDeuses = probabilidadeDeusesBasica;
+    }
+
+
+    if (option === "ajuda") return message.channel.send("**COMO FUNCIONA O CAÇAR?**\nVocê pode caçar a cada uma hora, e você pode escolher entre 4 caças: `demonios`, `anjos`, `semideuses` e `deuses`\nQuanto mais pra direita, mais valioso, mas também, mais dificil de se caçar, e menor a chance de sucesso\nEm meu servidor de suporte, as chances de caça são maiores!\n**Boas Caçadas**")
+    if (option === "probabilidades") return message.channel.send(`**PROBABILIDADES:**\nÉ selecionado um número aleatório dentro dos próximos. Cada número é a quantidade de caças\nDica: Em meu servidor de suporte, as chances de caça são maiores!\n\nDemônio: \`${probabilidadeDemonio}\`\nAnjo: \`${probabilidadeAnjo}\`\nSemideuses: \`${probabilidadeSD}\`\nDeuses: \`${probabilidadeDeuses}\``)
 
     if (parseInt(user.caçarTime) < Date.now()) {
       let avatar = message.author.displayAvatarURL({ format: "png", dynamic: true });
@@ -63,10 +92,10 @@ module.exports = {
         .setTitle("Caçada")
         .setColor("#faa40f")
         .setThumbnail(avatar)
+        .setFooter('Sabia que eu meu servidor de suporte, suas chances de sucesso são maiores?')
 
       switch (option) {
         case 'demônio':
-          const probabilidadeDemonio = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 4];
           const dc = probabilidadeDemonio[Math.floor(Math.random() * probabilidadeDemonio.length)];
           user.caçados = user.caçados + dc;
           user.caçarTime = 3600000 + Date.now();
@@ -75,7 +104,6 @@ module.exports = {
           message.channel.send(embed)
           break;
         case 'anjos':
-          const probabilidadeAnjo = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2];
           const da = probabilidadeAnjo[Math.floor(Math.random() * probabilidadeAnjo.length)];
           user.anjos = user.anjos + da;
           user.caçarTime = 3600000 + Date.now();
@@ -84,7 +112,6 @@ module.exports = {
           message.channel.send(embed)
           break;
         case 'semideuses':
-          const probabilidadeSD = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
           const ds = probabilidadeSD[Math.floor(Math.random() * probabilidadeSD.length)];
           user.semideuses = user.semideuses + ds;
           user.caçarTime = 3600000 + Date.now();
@@ -93,18 +120,18 @@ module.exports = {
           message.channel.send(embed)
           break;
         case 'deus':
-          const probabilidadeDeuses = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]; 
           const dd = probabilidadeDeuses[Math.floor(Math.random() * probabilidadeDeuses.length)];
           user.deuses = user.deuses + dd;
           user.caçarTime = 3600000 + Date.now();
           user.save()
-          if(dd > 0) embed.setColor('#e800ff')
+          if (dd > 0) embed.setColor('#e800ff')
           embed.setDescription((dd > 0) ? `CARACA!!! VOCÊ SE MOSTROU UM HERÓI FRUTO DE UMA VONTADE DIVINA, ASSIM COMO XANDÃO, E CONSEGUIU CAÇAR \`${dd}\` DEUS!!!!!!` : `Caçar Deuses é uma missão extremamente difícil, e você acabou não conseguindo, levando um total de \`${dd}\` deuses pra casa`)
           message.channel.send(embed)
           break;
       }
     } else {
-      message.channel.send(`<:negacao:759603958317711371> | Descanse campeão ${message.author}, você já saiu na sua caçada. Tente novamente em **${moment.utc(parseInt(user.caçarTime - Date.now())).format("mm:ss")}** minutos`)
+      message.channel.send(`<:negacao:759603958317711371> | Descanse campeão ${message.author}, você já saiu na sua caçada. Tente novamente em **${moment.utc(parseInt(user.caçarTime - Date.now())).format("mm:ss")}** minutos\n<:atencao:759603958418767922> | **DICA:** Sabia que em meu servidor de suporte, as chances de caça são maiores? m!suporte`)
     }
 
-  }};
+  }
+};
