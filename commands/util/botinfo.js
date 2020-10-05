0d58d4b399e1400f2edd2bb02266a3f122a6b808
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-05 20:34:14 -0300

    New BotInfo

--- commands/util/botinfo.js
+++ commands/util/botinfo.js
@@ -1,6 +1,9 @@
-const {MessageEmbed} = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
-const userModel = require("../../models/user.js");
+const config = require('../../config.json')
+const moment = require('moment')
+require("moment-duration-format")
+moment.locale('pt-br')
 
 module.exports = {
   name: "botinfo",
@@ -13,55 +16,46 @@ module.exports = {
   usage: "m!botinfo",
   run: async (client, message, args) => {
 
-  let avatar = message.author.displayAvatarURL({ format: "png" });
-  let days = Math.floor(client.uptime / 86400000);
-  let hours = Math.floor(client.uptime / 3600000) % 24;
-  let minutes = Math.floor(client.uptime / 60000) % 60;
-  let seconds = Math.floor(client.uptime / 1000) % 60;
-  let uptime = `${days}d, ${hours}h, ${minutes}m e ${seconds}s`;
-  if(days == 0 && hours != 0) uptime = `${hours}h, ${minutes}m e ${seconds}s`;
-  if(hours == 0) uptime = `${minutes}m e ${seconds}s`;
 
-let embed = new MessageEmbed()
-.setTitle(`📈 | Menhera Status`, client.user.displayAvatarURL())
-.setFooter(message.author.tag, avatar)
-.setTimestamp()
-.setColor('#eab3fa')
+const owner = await client.users.fetch(config.owner[0])
 
-
-userModel.countDocuments({}, function(err, count) {
-  if (err) console.log(err)
-  
-  embed.addFields([
-    {
-      name: "🌐 | Servers",
-      value: `${client.guilds.cache.size} `,
-      inline: true 
-    },
-    {
-      name: "🗄️ | Canais",
+const embed = new MessageEmbed()
+				.setColor('#fa8dd7')
+				.setThumbnail("https://i.imgur.com/b5y0nd4.png")
+				.setDescription(`Oi, meu nome é **${client.user.username}**, e eu sou uma Bot Brasileira para Discord com foco em diversão e RPG!\n\nEu sou feita em JavaScript com discord.js! Fui criada em **${moment.utc(client.user.createdAt).format("LLLL")}** e entrei nesse servidor **${moment.utc(message.guild.me.joinedAt).format("LLLL")}**`)
+        .setFooter(`${client.user.username} foi criada por ${owner.tag}`, owner.displayAvatarURL({ format: "png", dynamic: true }))
+        .addFields([
+          {
+            name: "🌐 | Servers",
+            value: `${client.guilds.cache.size} `,
+            inline: true 
+          },
+          {
+            name: "🗄️ | Canais",
             value: client.channels.cache.size,
             inline: true
-    },
-    {
-      name: "📊 | Usuários",
-      value: client.users.cache.size,
-      inline: true
-    }
-  ],
-  [  
-    {
-      name: "⏳ | Uptime",
-      value: uptime,
-      inline: true
-    },
-    {
-      name: "🧮 | Usuários na DB",
-      value: count,
-      inline: true
-    }
-  ]
-  )
-  message.channel.send(message.author, embed);
-})
+          },
+          {
+            name: "📊 | Usuários",
+            value: client.users.cache.size,
+            inline: true
+          },
+          {
+            name: "⏳ | Uptime",
+            value: moment.duration(client.uptime).format("D[d], H[h], m[m], s[s]"),
+            inline: true
+          },
+          {
+            name: "<:memoryram:762817135394553876> | Memória",
+            value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`,
+            inline: true
+          },
+          {
+            name: "🇧🇷 | Versão",
+            value: require("../../package.json").version,
+            inline: true
+          }
+        ])
+        message.channel.send(embed)
+
 }};
