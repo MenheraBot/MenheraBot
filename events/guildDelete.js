d49a875fbfb2aeb8401527bea6b6c29bccfc4e13
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-05 19:47:30 -0300

    Propery deleting guilds from DB

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,6 +2,13 @@ const serverDb = require("../models/guild.js")
 
 module.exports = (client, server) => {
 
-    serverDb.findOneAndDelete({id: server.id}).then(res => console.log(`GuildDB deleted! ID: ${res.id}`));
+    serverDb.findOneAndDelete({id: server.id }, function (err, docs) { 
+        if (err){ 
+            console.log(err) 
+        } 
+        else{ 
+            console.log("[EVENT] Deleted Guild: " + docs.id); 
+        } 
+    }); 
     client.guilds.cache.get('717061688460967988').channels.cache.get('717061688729534628').send(`<:menhera_cry:744041825140211732> | Fui removida do servidor **${server}**`)
 }
