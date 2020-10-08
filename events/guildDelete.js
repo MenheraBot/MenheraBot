4e05cdcad3b5d10cb0cb67de2ce4dce500fa54da
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-08 16:21:17 -0300

    Error

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,7 +3,7 @@ const serverDb = require("../models/guild.js")
 module.exports = (client, server) => {
 
     serverDb.findOneAndDelete({id: server.id }, function (err, docs) { 
-     console.log("[EVENT] Deleted Guild: " + server.id).catch()
+     console.log("[EVENT] Deleted Guild: " + server.id)
     }); 
     client.guilds.cache.get('717061688460967988').channels.cache.get('717061688729534628').send(`<:menhera_cry:744041825140211732> | Fui removida do servidor **${server}**`)
 }
