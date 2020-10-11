ed223a69251140be8af8faf247f7483febf27a86
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-11 15:15:50 -0300

    Reset RPG at lvl 7

---
+++ commands/rpg/RpgResetCommand.js
@@ -0,0 +1,36 @@
+const database = require("../../models/rpg.js");
+
+module.exports = {
+    name: "reset",
+    aliases: [],
+    cooldown: 3,
+    category: "rpg",
+    dir: 'RpgResetCommand',
+    description: "Reseta seu perfil do RPG",
+    userPermission: null,
+    clientPermission: ["EMBED_LINKS"],
+    usage: "m!reset",
+    run: async (client, message, args) => {
+
+        const user = await database.findById(message.author.id)
+        if (!user) return message.channel.send("<:negacao:759603958317711371> | Você não é um aventureiro!")
+        if(user.level < 7) return message.channel.send("<:negacao:759603958317711371> | Você precisa estar no nível **7** para poder resetar")
+
+        message.channel.send("<:atencao:759603958418767922> | Você realmente deseja resetar seu perfil do RPG? Esta ação é **IRREVERSÍVEL**\nDigite `sim` para confirmar")
+
+        const filter = m => m.author.id === message.author.id;
+        const collector = message.channel.createMessageCollector(filter, {
+            max: 1,
+            time: 30000,
+            errors: ["time"]
+        });
+
+        collector.on('collect', m => {
+            
+            if(m.content.toLowerCase() == "sim"){
+                database.findByIdAndDelete(message.author.id).then(message.channel.send("<:positivo:759603958485614652> | Você resetou com sucesso sua conta do RPG! Para jogar novamente, use m!registrar"))
+            } else message.channel.send("<:negacao:759603958317711371> | Sua conta **não** foi resetada!")
+        
+        })
+    }
+}
