f15d3c4989d0248e1c5f30148bb19ecc1be0469d
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-22 00:54:49 -0300

    Teste final

--- src/commands/Dev/EvalCommand.js
+++ src/commands/Dev/EvalCommand.js
@@ -11,6 +11,8 @@ module.exports = class EvalCommand extends Command {
     }
     async run(message, args) {
 
+        return message.reply("yay")
+
         try {
             const util = require("util")
             let evaled = await eval(args.join(" "))
