5a3cc637b784f4925ce2026697e7ab81378cee2a
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-25 16:29:46 -0300

    pm2 configuratin file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [{
+        name: "Menhera",
+        script: "./index.js",
+        watch: false,
+        max_memory_restart: "768M",
+        error_file: './logs/err.log',
+        out_file: './logs/out.log',
+        log_file: './logs/combined.log',
+        interpreter_args: "--max-old-space-size=768"
+    }]
+}
