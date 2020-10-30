24fe38832fe409b699a8d45caebe9b79aa96ac2b
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-10-30 10:32:35 -0300

    :bug: | Fix: O TOKEN KK

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.loadLocales()
 client.loadCommands("./src/commands")
 client.loadEvents("./src/events")
 
-client.login(config.testToken).then(() => {
+client.login(config.token).then(() => {
     console.log("[INDEX] Logged in")
 }).catch((e) => console.log(`[FATALERROR] Failure connecting to Discord! ${e.message}!`))
 
