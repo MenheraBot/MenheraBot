4853d920bb9f5b2444d1ac6d3c8ee34682be0322
ySnoopyDogy YT <CommandPlayermine@gmail.com>
2020-11-04 14:10:36 -0300

    :tada: | New: Novo sistema de poções

--- src/commands/info/UpdatesCommand.js
+++ src/commands/info/UpdatesCommand.js
@@ -19,12 +19,11 @@ module.exports = class UpdatesCommand extends Command {
             .setTitle(`${t("commands:updates.title")} ${version}`)
             .setColor('#a7e74f')
             .setFooter(`${this.client.user.username} ${t("commands:updates.footer")} ${owner.tag}`, owner.displayAvatarURL({ format: "png", dynamic: true }))
-            .setDescription(`**Comando Top**
-
-            • Agora é possível escolher a posição do top que desejas ver
-            Exemplo: \`m!top estrelinhas 3\` - Vai mostrar a página 3 do top de estrelinhas
+            .setDescription(`**POÇÕES**
+            • Mudanças no sistema de poções:
+            Agora os níveis maximos voltaram!
+            Adicionado mais poções a partir do nivel 25
         `)
-
 
         message.channel.send(message.author, embed)
     }
