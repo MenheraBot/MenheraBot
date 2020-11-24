4d08dfb2d3919da49e029c94b735ed8890b2e2c9
Luxanna <CommandPlayermine@gmail.com>
2020-11-24 13:36:06 -0300

    :star: | Comando de update

--- src/commands/info/UpdatesCommand.js
+++ src/commands/info/UpdatesCommand.js
@@ -20,40 +20,24 @@ module.exports = class UpdatesCommand extends Command {
       .setTitle(`${t('commands:updates.title')} ${version}`)
       .setColor('#a7e74f')
       .setFooter(`${this.client.user.username} ${t('commands:updates.footer')} ${owner.tag}`, owner.displayAvatarURL({ format: 'png', dynamic: true }))
-      .setDescription(`**Nível 30 do RPG**
+      .setDescription(`**Buffs no mundo de Boleham**
 
-            • Agora ao atingir o nível 30, tu evolui para uma nova classe e ganha uma nova habilidade:
-
-            Assassino -> Senhor das Sombras
-                 • Habilidade: \`Sombras a Caminho\` - Envia as sombras para cegar seu inimigo
-
-            Bárbaro -> Berserker
-                • Habildiade: \`Frenesi\` - Entra em modo Frenesi, curando e causando dano a seus inimigos
-
-            Clérigo -> Arcanjo
-                • Habilidade: \`Manipulação Éterea\` - Envia os mensageiros da Luz para aniquilar seus inimigos
-
-            Druida -> Guardião da Natureza
-                • Habilidade: \`Chamado da Natuzera\` - Chama os seus aliados naturais para acabar com o inimigo
-
-            Espadachim -> Mestre das Armas
-                • Habilidade: \`Golpe das 7 Lâminas\` - Invoca todas as armas de sua alma, desferindo inúmeros golpes simultâneos
-
-            Feiticeiro/místico -> Senhor das Galáxias
-               • Habilidade: \`Super Nova\` - Cria uma supernova de força inabalável, expurgando seus inimigos
-
-            Feiticeiro/dracônico -> Mestre dos Elementos
-                • Habilidade: \`Força dos 5 Elementos\` - Conjura as forças dos 5 dragões elementais
-
-            Feiticeiro/demoníaco -> Conjurador Demoníaco
-                • Habilidade: \`Pentagrama Mundial\` - Fecha o Pentagrama entre os 5 Dragões Elementais, trazendo as forças do fim do mundo
-
-            Monge -> Sacerdote
-                • Habilidade: \`Guerra Espiritual\` - Invade a mente de seu inimigo, colocando-o fazendo com que sua mente luta contra si mesmo
-
-            Necromante -> Senhor das Trevas
-                • Habilidade: \`Fim dos Tempos\` - Todos os mortos do mundo aparecem para acabar com seu inimigo
-        `);
+      • Espadachim: 
+          Habilidade 'Golpe Duplo' dano de \`14\` para \`24\`
+          Habilidade 'Combate Tático' dano de \`35\` para \`48\`
+          Habilidade 'Excalibur' dano de \`55\` para \`72\`
+          Habilidade 'Kenjutsu' dano de \`90\` para \`100\` 
+          Habilidade 'Golpe das 7 lâminas' dano de \`130\` para \`132\`
+      
+      • Clérigo
+          Habilidade 'Cura' dano de \`0\` para \`2\`
+          Habilidade 'Raio de Luz Solar' dano de \`20\` para \`30\`
+          Habilidade 'Rosário' dano de \`40\` para \`50\`
+          Habilidade 'Ascenção Espiritual' dano de \`46\` para \`66\`
+          Habilidade 'Manipulação Éterea' dano de \`62\` para\`86\`
+      
+      • Assassino
+          Habilidade 'Assassino em Série' custo de \`35\` para \`25\``);
 
     message.channel.send(message.author, embed);
   }
