16bbf93ebdf690fc17a97de71cbf6ecd943de439
Luxanna <CommandPlayermine@gmail.com>
2020-12-20 05:40:47 -0300

    :ambulance: | TOKEN AGAIN

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ client.init();
 client.loadCommands('src/commands');
 client.loadEvents('src/events');
 
-client.login(config.testToken)
+client.login(config.token)
   .then(() => console.log('[INDEX] Logged in'))
   .catch((e) => console.log(`[FATALERROR] Failure connecting to Discord! ${e.message}!`));
