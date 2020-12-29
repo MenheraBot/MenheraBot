db690b7321b07a85eb2121031606959144c9289d
Luxanna <CommandPlayermine@gmail.com>
2020-12-29 11:02:15 -0300

    :heavy_plus_sign: | Add canvas

--- src/commands/Dev/TestCommand.js
+++ src/commands/Dev/TestCommand.js
@@ -5,7 +5,7 @@ module.exports = class TestCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'test',
-      aliases: ['natal'],
+      aliases: ['pcm'],
       description: 'Arquivo destinado para testes',
       devsOnly: true,
       category: 'Dev',
@@ -13,15 +13,6 @@ module.exports = class TestCommand extends Command {
   }
 
   async run({ message, args }) {
-    const user = await this.client.users.fetch(args[0]);
-    const canal = this.client.channels.cache.get('717061688729534632');
-    const db = await this.client.database.Users.findOne({ id: args[0] });
 
-    db.estrelinhas += 50000;
-    db.rolls += 20;
-    db.save();
-
-    message.channel.send(`Presenteado ${user}`);
-    canal.send(`**Feliz natal ${user}**\nVocê recebeu **50000** estrelinhas e **20** rolls de presente!`);
   }
 };
