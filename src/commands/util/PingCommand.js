be2ef645d0c8b372010ed4830a228c5f5f2ec5bb
Luxanna <CommandPlayermine@gmail.com>
2021-01-15 23:16:48 -0300

    :star: | Shardssssssss

--- src/commands/util/PingCommand.js
+++ src/commands/util/PingCommand.js
@@ -11,15 +11,27 @@ module.exports = class PingCommand extends Command {
     });
   }
 
-  async run({ message }, t) {
+  async run({ message, args }, t) {
     const avatar = message.author.displayAvatarURL({ format: 'png', dynamic: true });
 
-    const embed = new MessageEmbed()
-      .setTitle('🏓 | Pong!')
-      .setDescription(`📡 | ${t('commands:ping.api')} **${Date.now() - message.createdTimestamp}ms**\n📡 | ${t('commands:ping.latency')} **${Math.round(this.client.ws.ping)}ms**`)
-      .setFooter(message.author.tag, avatar)
-      .setTimestamp()
-      .setColor('#eab3fa');
+    let embed;
+
+    if (args[0] === 'shard' || args[0] === 'shards') {
+      this.client.shard.broadcastEval('this.ws.ping').then((shard) => {
+        embed = new MessageEmbed()
+          .setColor('#eab3fa')
+          .setFooter(`${t('commands:ping')} ${this.client.shard.count} shards`);
+        shard.forEach((ping, index) => embed.addField(`Shard **${index}**`, `**${Math.round(ping)}**ms`, true));
+        message.channel.send(embed);
+      });
+    } else {
+      embed = new MessageEmbed()
+        .setTitle('🏓 | Pong!')
+        .setDescription(`📡 | ${t('commands:ping.api')} **${Date.now() - message.createdTimestamp}ms**\n📡 | ${t('commands:ping.latency')} **${Math.round(this.client.ws.ping)}ms**\n🖲️ | ShardId: [**${this.client.shard.ids}**/${this.client.shard.count}]`)
+        .setFooter(message.author.tag, avatar)
+        .setTimestamp()
+        .setColor('#eab3fa');
+    }
 
     message.channel.send(embed);
   }
