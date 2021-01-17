7b6622337c1680af1ec044af676494727d73d41d
Luxanna <CommandPlayermine@gmail.com>
2021-01-17 00:52:48 -0300

    :bug: | Fix ping shards

--- src/commands/util/PingCommand.js
+++ src/commands/util/PingCommand.js
@@ -14,25 +14,23 @@ module.exports = class PingCommand extends Command {
   async run({ message, args }, t) {
     const avatar = message.author.displayAvatarURL({ format: 'png', dynamic: true });
 
-    let embed;
-
     if (args[0] === 'shard' || args[0] === 'shards') {
       this.client.shard.broadcastEval('this.ws.ping').then((shard) => {
-        embed = new MessageEmbed()
+        const embed = new MessageEmbed()
           .setColor('#eab3fa')
           .setFooter(message.author.tag, avatar);
         shard.forEach((ping, index) => embed.addField(`Shard **${index}**`, `**${Math.round(ping)}**ms`, true));
-        message.channel.send(embed);
+        return message.channel.send(embed);
       });
     } else {
-      embed = new MessageEmbed()
+      const embed = new MessageEmbed()
         .setTitle('🏓 | Pong!')
         .setDescription(`📡 | ${t('commands:ping.api')} **${Date.now() - message.createdTimestamp}ms**\n📡 | ${t('commands:ping.latency')} **${Math.round(this.client.ws.ping)}ms**\n🖲️ | ShardId: [**${this.client.shard.ids}**/${this.client.shard.count - 1}]`)
         .setFooter(message.author.tag, avatar)
         .setTimestamp()
         .setColor('#eab3fa');
-    }
 
-    message.channel.send(embed);
+      return message.channel.send(embed);
+    }
   }
 };
