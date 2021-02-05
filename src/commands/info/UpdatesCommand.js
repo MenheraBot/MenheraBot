2cee8ef5402d422dbc63486e207c3c1a491f9b28
Luxanna <CommandPlayermine@gmail.com>
2021-02-05 14:30:39 -0300

    :star: | Version

--- src/commands/info/UpdatesCommand.js
+++ src/commands/info/UpdatesCommand.js
@@ -20,14 +20,15 @@ module.exports = class UpdatesCommand extends Command {
       .setTitle(`${t('commands:updates.title')} ${version}`)
       .setColor('#a7e74f')
       .setFooter(`${this.client.user.username} ${t('commands:updates.footer')} ${owner.tag}`, owner.displayAvatarURL({ format: 'png', dynamic: true }))
-      .setDescription(`**As famílias foram removidas**
+      .setDescription(`**Trabalhos**
 
-      • Certamente as famílias eram desbalanceadas, e elas não fluiram como eu pensei que seria! Então, removi elas!
-      Para as pessoas que gastaram dinheiro nos depósitos: kk dinheiro no lixo
+      • O RPG agora possui trabalhos! Escolha um trabalho com \`m!job\` e vá trabalhar com \`m!work\`.
+      • Usar o \`m!job\` sem argumentos mostrará todos os trabalhos para escolher (Por enquanto só tem 2 por que quero ver se ta tudo 100%, vou lançar mais com o tempo).
+      • Use \`m!job [id do trabalho]\` para entrar nesse trabalho!
 
-      • Novidades futuras:
-        Para substituir as famílias, o mundo de Boleham será ampliado para não ter somente Dungeons, mas também, ter trabalhos, famílias próprias, e casas!
-      (é o que eu pretendo fazer, se conseguir keke)`);
+      Cada trabalho tem um certo número de cooldown, pedras magicas, itens, e xp. Os trabalhos são 100% traduzidos, tanto os itens quanto os próprios trabalhos, quero fazer isso com todo o rpg ainda, mas vai dar trabalho ja que o rpg é todo torto e errado
+
+      É isso, essa vai ser uma forma a mais de ganhar coisas pra se fortalecer no Mundo de Bolham, que está pronto para crescer mais e mais`);
 
     message.channel.send(message.author, embed);
   }
