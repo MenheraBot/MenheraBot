031c5e0e9757f02080a8e75b2119ae16c3c73865
Luxanna <CommandPlayermine@gmail.com>
2021-04-29 16:49:12 -0300

    :green_heart: | remove log files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,6 @@ module.exports = {
     script: './shard.js',
     watch: false,
     max_memory_restart: '768M',
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
     interpreter_args: '--max-old-space-size=768',
   }],
 };
