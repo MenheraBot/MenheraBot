cec30be802599b1aa447bd2c74e0863bc3e3ef5e
Luxanna <CommandPlayermine@gmail.com>
2021-05-25 00:12:58 -0300

    :bug: | Fix audio command

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import config from '../config.json';
 ProtoTypes.start();
 
 const client = new MenheraClient(
-  { disableMentions: 'everyone', messageCacheMaxSize: 50, ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] } },
+  { disableMentions: 'everyone', messageCacheMaxSize: 50, ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_VOICE_STATES'] } },
   {
     commandsDirectory: resolve(__dirname, 'commands'),
     eventsDirectory: resolve(__dirname, 'events'),
