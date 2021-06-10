54f84788305305a4ffb9fff6e5f4065481ae9228
Luxanna <CommandPlayermine@gmail.com>
2021-06-09 22:05:59 -0300

    :star: | update Succumb command

--- src/commands/fun/SuccumbCommand.js
+++ src/commands/fun/SuccumbCommand.js
@@ -8,10 +8,10 @@ module.exports = class SuccumbCommand extends Command {
     });
   }
 
-  async run({ message, args }, t) {
-    const user = message.mentions.users.first() || args.join(' ');
-    if (!user) return message.menheraReply('error', 'n/a');
-    if (user.id === message.author.id) return message.menheraReply('error', 'n/a');
-    message.channel.send(`${t('commands:sucumba.start')} **${user}** ${t('commands:sucumba.end')}`);
+  async run(ctx) {
+    const user = ctx.message.mentions.users.first() || ctx.args.join(' ');
+    if (!user) return ctx.reply('error', 'n/a');
+    if (user.id === ctx.message.author.id) return ctx.reply('error', 'n/a');
+    ctx.send(`${ctx.locale('commands:sucumba.start')} **${user}** ${ctx.locale('commands:sucumba.end')}`);
   }
 };
