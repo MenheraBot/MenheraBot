c4138521dd66d1b153db49d9420e332803c12775
Luxanna <CommandPlayermine@gmail.com>
2021-06-10 00:27:37 -0300

    :star: | Update support  Command

--- src/commands/util/SuportCommand.js
+++ src/commands/util/SuportCommand.js
@@ -12,14 +12,14 @@ module.exports = class SuportCommand extends Command {
     });
   }
 
-  async run({ message }, t) {
+  async run(ctx) {
     const embed = new MessageEmbed()
-      .setTitle(t('commands:support.embed_title'))
+      .setTitle(ctx.locale('commands:support.embed_title'))
       .setURL('https://discord.gg/fZMdQbA')
       .setColor('#970045')
       .setImage('https://i.imgur.com/ZsKuh8W.png')
-      .setFooter(`${t('commands:support.embed_footer')} ${message.author.tag}`, message.author.displayAvatarURL())
+      .setFooter(`${ctx.locale('commands:support.embed_footer')} ${ctx.message.author.tag}`, ctx.message.author.displayAvatarURL())
       .setTimestamp();
-    message.channel.send(message.author, embed);
+    ctx.sendC(ctx.message.author, embed);
   }
 };
