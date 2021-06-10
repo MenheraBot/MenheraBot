7ade1dfe92b0ab0e4aad046f630184955275f164
Luxanna <CommandPlayermine@gmail.com>
2021-06-10 15:06:09 -0300

    :star: | Update reset Command

--- src/commands/rpg/RpgResetCommand.js
+++ src/commands/rpg/RpgResetCommand.js
@@ -10,20 +10,20 @@ module.exports = class RpgResetCommand extends Command {
     });
   }
 
-  async run({ message, server }, t) {
-    const user = await this.client.database.Rpg.findById(message.author.id);
-    if (!user) return message.menheraReply('error', t('commands:reset.non-aventure'));
-    if (user.level < 4) return message.menheraReply('error', t('commands:reset.low-level'));
+  async run(ctx) {
+    const user = await this.client.database.Rpg.findById(ctx.message.author.id);
+    if (!user) return ctx.replyT('error', 'commands:reset.non-aventure');
+    if (user.level < 4) return ctx.replyT('error', 'commands:reset.low-level');
 
-    message.menheraReply('warn', t('commands:reset.confirm'));
+    ctx.replyT('warn', 'commands:reset.confirm');
 
-    const filter = (m) => m.author.id === message.author.id;
-    const collector = message.channel.createMessageCollector(filter, { max: 1, time: 30000, errors: ['time'] });
+    const filter = (m) => m.author.id === ctx.message.author.id;
+    const collector = ctx.message.channel.createMessageCollector(filter, { max: 1, time: 30000, errors: ['time'] });
 
     collector.on('collect', async (m) => {
       if (m.content.toLowerCase() === 'sim' || m.content.toLowerCase() === 'yes') {
-        this.client.database.Rpg.findByIdAndDelete(message.author.id).then(message.menheraReply('success', t('commands:reset.success', { prefix: server.prefix })));
-      } else message.menheraReply('error', t('commands:reset.cancel'));
+        this.client.database.Rpg.findByIdAndDelete(ctx.message.author.id).then(ctx.replyT('success', 'commands:reset.success', { prefix: ctx.data.server.prefix }));
+      } else ctx.replyT('error', 'commands:reset.cancel');
     });
   }
 };
