cc8859f2df6f8d47add39d84b87c4915258368b2
Luxanna <CommandPlayermine@gmail.com>
2021-06-10 00:15:29 -0300

    :star: | update help command

--- src/commands/util/HelpCommand.js
+++ src/commands/util/HelpCommand.js
@@ -13,13 +13,13 @@ module.exports = class HelpCommand extends Command {
     });
   }
 
-  async run({ message, args, server }, t) {
-    if (args[0]) {
+  async run(ctx) {
+    if (ctx.args[0]) {
       // eslint-disable-next-line no-use-before-define
-      return getCMD(this.client, message, args[0], t, server);
+      return getCMD(ctx);
     }
     // eslint-disable-next-line no-use-before-define
-    return getAll(this.client, message, t, server);
+    return getAll(ctx);
   }
 };
 
@@ -31,43 +31,43 @@ function getCategory(category, client, server) {
   return client.commands.filter((c) => c.config.category === category).map((c) => `\`${server.prefix}${c.config.name}\``).join(', ');
 }
 
-function getAll(client, message, t, server) {
+function getAll(ctx) {
   const embed = new MessageEmbed();
   embed.setColor('#b880e6');
-  embed.setThumbnail(client.user.displayAvatarURL());
+  embed.setThumbnail(ctx.client.user.displayAvatarURL());
 
-  embed.addField(`${t('commands:help.actions')} (${getCommmandSize('ações', client)})`, getCategory('ações', client, server));
-  embed.addField(`${t('commands:help.fun')} (${getCommmandSize('diversão', client)})`, getCategory('diversão', client, server));
-  embed.addField(`${t('commands:help.economy')} (${getCommmandSize('economia', client)})`, getCategory('economia', client, server));
-  embed.addField(`${t('commands:help.info')} (${getCommmandSize('info', client)})`, getCategory('info', client, server));
-  embed.addField(`${t('commands:help.mod')} (${getCommmandSize('moderação', client)})`, getCategory('moderação', client, server));
-  embed.addField(`${t('commands:help.rpg')} (${getCommmandSize('rpg', client)})`, getCategory('rpg', client, server));
-  embed.addField(`${t('commands:help.util')} (${getCommmandSize('util', client)})`, getCategory('util', client, server));
+  embed.addField(`${ctx.locale('commands:help.actions')} (${getCommmandSize('ações', ctx.client)})`, getCategory('ações', ctx.client, ctx.data.server));
+  embed.addField(`${ctx.locale('commands:help.fun')} (${getCommmandSize('diversão', ctx.client)})`, getCategory('diversão', ctx.client, ctx.data.server));
+  embed.addField(`${ctx.locale('commands:help.economy')} (${getCommmandSize('economia', ctx.client)})`, getCategory('economia', ctx.client, ctx.data.server));
+  embed.addField(`${ctx.locale('commands:help.info')} (${getCommmandSize('info', ctx.client)})`, getCategory('info', ctx.client, ctx.data.server));
+  embed.addField(`${ctx.locale('commands:help.mod')} (${getCommmandSize('moderação', ctx.client)})`, getCategory('moderação', ctx.client, ctx.data.server));
+  embed.addField(`${ctx.locale('commands:help.rpg')} (${getCommmandSize('rpg', ctx.client)})`, getCategory('rpg', ctx.client, ctx.data.server));
+  embed.addField(`${ctx.locale('commands:help.util')} (${getCommmandSize('util', ctx.client)})`, getCategory('util', ctx.client, ctx.data.server));
 
-  embed.addField(t('commands:help.link_name'), t('commands:help.link_value'));
+  embed.addField(ctx.locale('commands:help.link_name'), ctx.locale('commands:help.link_value'));
 
-  message.author.send(embed).then(() => {
-    message.menheraReply('success', t('commands:help.dm_sent'));
+  ctx.message.author.send(embed).then(() => {
+    ctx.replyT('success', 'commands:help.dm_sent');
   }).catch(() => {
-    message.menheraReply('error', t('commands:help.dm_error'));
+    ctx.replyT('error', 'commands:help.dm_error');
   });
 }
 
-function getCMD(client, message, input, t, server) {
+function getCMD(ctx) {
   const embed = new MessageEmbed();
 
-  const cmd = client.commands.get(input.toLowerCase()) || client.commands.get(client.aliases.get(input.toLowerCase()));
+  const cmd = ctx.client.commands.get(ctx.args[0].toLowerCase()) || ctx.client.commands.get(ctx.client.aliases.get(ctx.args[0].toLowerCase()));
 
-  let info = t('commands:help.without-info', { cmd: input.toLowerCase() });
+  let info = ctx.locale('commands:help.without-info', { cmd: ctx.args[0].toLowerCase() });
 
-  if (!cmd) return message.channel.send(embed.setColor('#ff0000').setDescription(info));
+  if (!cmd) return ctx.send(embed.setColor('#ff0000').setDescription(info));
 
-  info = `**${t('commands:help.cmd')}**: ${Util.captalize(cmd.config.name)}`;
-  if (cmd.config.aliases.length > 0) info += `\n**${t('commands:help.aliases')}**: ${cmd.config.aliases.map((a) => `\`${a}\``).join(', ')}`;
-  info += `\n**${t('commands:help.desc')}**: ${t(`commands:${cmd.config.name}.description`)}`;
-  info += `\n**Cooldown**: ${cmd.config.cooldown} ${t('commands:help.seconds')}`;
-  info += `\n**${t('commands:help.howTo')}**: ${server.prefix}${cmd.config.name} ${t(`commands:${cmd.config.name}.usage`)}`;
-  embed.setFooter(t('commands:help.footer'));
+  info = `**${ctx.locale('commands:help.cmd')}**: ${Util.captalize(cmd.config.name)}`;
+  if (cmd.config.aliases.length > 0) info += `\n**${ctx.locale('commands:help.aliases')}**: ${cmd.config.aliases.map((a) => `\`${a}\``).join(', ')}`;
+  info += `\n**${ctx.locale('commands:help.desc')}**: ${ctx.locale(`commands:${cmd.config.name}.description`)}`;
+  info += `\n**Cooldown**: ${cmd.config.cooldown} ${ctx.locale('commands:help.seconds')}`;
+  info += `\n**${ctx.locale('commands:help.howTo')}**: ${ctx.data.server.prefix}${cmd.config.name} ${ctx.locale(`commands:${cmd.config.name}.usage`)}`;
+  embed.setFooter(ctx.locale('commands:help.footer'));
 
-  return message.channel.send(embed.setColor('#00ffe1').setDescription(info));
+  return ctx.send(embed.setColor('#00ffe1').setDescription(info));
 }
