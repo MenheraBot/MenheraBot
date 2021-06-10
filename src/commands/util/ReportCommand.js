767999a9934ed2b945e8afcda4dd1dd17ee95bcf
Luxanna <CommandPlayermine@gmail.com>
2021-06-10 00:17:06 -0300

    :star: | update repotr command

--- src/commands/util/ReportCommand.js
+++ src/commands/util/ReportCommand.js
@@ -12,25 +12,25 @@ module.exports = class ReportCommand extends Command {
     });
   }
 
-  async run({ message, args }, t) {
-    const argumentos = args.join(' ');
+  async run(ctx) {
+    const argumentos = ctx.args.join(' ');
     const cor = `#${(`000000${Math.random().toString(16).slice(2, 8).toUpperCase()}`).slice(-6)}`;
 
-    if (!argumentos) return message.menheraReply('error', t('commands:report.no-args'));
+    if (!argumentos) return ctx.replyT('error', 'commands:report.no-args');
 
     const embed = new MessageEmbed()
       .setDescription(`${argumentos}`)
       .setColor(cor)
-      .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-      .setFooter(`ID do usuário: ${message.author.id}`)
+      .setThumbnail(ctx.message.author.displayAvatarURL({ dynamic: true }))
+      .setFooter(`ID do usuário: ${ctx.message.author.id}`)
       .setTimestamp()
-      .setAuthor(`Novo Bug Reportado por ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }));
+      .setAuthor(`Novo Bug Reportado por ${ctx.message.author.tag}`, ctx.message.author.displayAvatarURL({ dynamic: true }));
 
     const reportWebhook = await this.client.fetchWebhook(process.env.BUG_HOOK_ID, process.env.BUG_HOOK_TOKEN);
 
     reportWebhook.send(embed);
 
-    if (message.deletable) message.delete();
-    message.menheraReply('success', t('commands:report.thanks'));
+    if (ctx.message.deletable) ctx.message.delete();
+    ctx.replyT('success', 'commands:report.thanks');
   }
 };
