1a31b977fdb91bf80f8d8a71fe3e670167f8c4e4
Luxanna <CommandPlayermine@gmail.com>
2021-06-14 18:42:12 -0300

    :card_file_box: | Only Main bot update the status

--- src/events/ready.js
+++ src/events/ready.js
@@ -10,16 +10,15 @@ module.exports = class ReadyEvent {
     if (this.client.user.id === '708014856711962654' && this.client.shard.ids[0] === 0) {
       const DiscordBotList = new Dbl(this.client);
       DiscordBotList.init();
+      setInterval(async () => {
+        const shardId = this.client.shard.ids[0];
+        const atividade = await http.getActivity(shardId);
+        this.client.user.setPresence({ activity: atividade });
+        this.client.repositories.statusRepository.CreateOrUpdate(shardId, this.client.ws.ping, Date.now(), this.client.guilds.cache.size, `${this.client.uptime}`);
+      }, 1000 * 60);
     }
     this.client.user.setActivity('🥱 | Acabei de acoidar :3');
 
     console.log('[READY] Menhera se conectou com o Discord!');
-
-    setInterval(async () => {
-      const shardId = this.client.shard.ids[0];
-      const atividade = await http.getActivity(shardId);
-      this.client.user.setPresence({ activity: atividade });
-      this.client.repositories.statusRepository.CreateOrUpdate(shardId, this.client.ws.ping, Date.now(), this.client.guilds.cache.size, `${this.client.uptime}`);
-    }, 1000 * 60);
   }
 };
