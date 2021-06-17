e8e4e41c45a883c4116ef8fdd92a9a6df619c768
Luxanna <CommandPlayermine@gmail.com>
2021-06-16 23:17:33 -0300

    :star: | Fix status

--- src/events/ready.js
+++ src/events/ready.js
@@ -19,23 +19,32 @@ module.exports = class ReadyEvent {
     console.log('[READY] Menhera se conectou com o Discord!');
 
     setInterval(async () => {
-      const shardId = this.client.shard.ids[0];
-      const atividade = await http.getActivity(shardId);
+      const atividade = await http.getActivity(this.client.shard.ids[0]);
       this.client.user.setPresence({
         activity: atividade,
       });
-      const ping = await this.client.database.Status.findById(shardId);
-      if (!ping) {
-        const newShard = new this.client.database.Status({
-          _id: shardId, ping: this.client.ws.ping, guilds: this.client.guilds.cache.size, uptime: this.client.uptime, lastPingAt: Date.now(),
+
+      if (this.client.user.id === '708014856711962654' && this.client.shard.ids[0] === 0) {
+        const allShardsPing = await this.client.shard.broadcastEval('this.ws.ping');
+        const allShardsUptime = await this.client.shard.broadcastEval('this.ws.client.uptime');
+        const guildsPerShardCount = await this.client.shard.broadcastEval('this.guilds.cache.size');
+
+        allShardsPing.map(async (shardPing, id) => {
+          const ping = await this.client.database.Status.findById(id);
+
+          if (!ping) {
+            const newShard = new this.client.database.Status({
+              _id: id, ping: shardPing, guilds: guildsPerShardCount[id], uptime: allShardsUptime[id], lastPingAt: Date.now(),
+            });
+            await newShard.save();
+          } else {
+            ping.ping = shardPing;
+            ping.lastPingAt = Date.now();
+            ping.guilds = guildsPerShardCount[id];
+            ping.uptime = `${allShardsUptime[id]}`;
+            await ping.save();
+          }
         });
-        await newShard.save();
-      } else {
-        ping.ping = this.client.ws.ping;
-        ping.lastPingAt = Date.now();
-        ping.guilds = this.client.guilds.cache.size;
-        ping.uptime = `${this.client.uptime}`;
-        await ping.save();
       }
     }, 1000 * 60);
   }
