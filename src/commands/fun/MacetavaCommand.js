6fe338e65d45d477b5bca206e93c97d5bf72ebf1
Luxanna <CommandPlayermine@gmail.com>
2021-06-24 23:48:25 -0300

    :sparkles: | Add macetava command

---
+++ src/commands/fun/MacetavaCommand.js
@@ -0,0 +1,32 @@
+const { MessageAttachment } = require('discord.js');
+const Command = require('../../structures/command');
+const NewHttp = require('../../utils/NewHttp');
+
+module.exports = class MacetavaCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'macetava',
+      cooldown: 10,
+      category: 'diversão',
+      clientPermissions: ['ATTACH_FILES'],
+    });
+  }
+
+  async run(ctx) {
+    let link = ctx.message.author.displayAvatarURL({ format: 'png', size: 512 });
+
+    if (ctx.message.mentions.users.first()) link = ctx.message.mentions.users.first().displayAvatarURL({ format: 'png', size: 512 });
+
+    if (ctx.message?.reference?.messageID) {
+      const fetchedMessage = await ctx.message.channel.messages.fetch(ctx.message.reference.messageID);
+      if (fetchedMessage.attachments.first()) link = fetchedMessage.attachments.first().url;
+    }
+
+    if (ctx.message.attachments.first()) link = ctx.message.attachments.first().url;
+
+    const res = await NewHttp.macetavaRequest(link);
+    if (res.err) return ctx.replyT('error', 'commands:http-error');
+
+    ctx.sendC(ctx.message.author, new MessageAttachment(Buffer.from(res.data), 'macetava.png'));
+  }
+};
