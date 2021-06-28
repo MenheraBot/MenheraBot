3582a7560c9f7f66c5952f537aa2ea91bec5d49c
Luxanna <CommandPlayermine@gmail.com>
2021-06-28 15:03:47 -0300

    :fire: | Delete warn system

--- src/structures/DatabaseCollections.js
+++ src/structures/DatabaseCollections.js
@@ -92,14 +92,6 @@ const userSchema = mongoose.Schema({
   }
 */
 
-const warnSchema = mongoose.Schema({
-  userId: String,
-  warnerId: String,
-  guildId: String,
-  reason: String,
-  data: String,
-});
-
 const commandsSchema = mongoose.Schema({
   name: { type: String },
   pt_description: { type: String },
@@ -115,7 +107,6 @@ const status = mongoose.model('status', statusSchema);
 const guild = mongoose.model('guild', guildSchema);
 const rpg = mongoose.model('rpg', rpgSchema);
 const user = mongoose.model('usersdb', userSchema);
-const warn = mongoose.model('warn', warnSchema);
 
 module.exports.Cmds = cmd;
 module.exports.Commands = commands;
@@ -123,4 +114,3 @@ module.exports.Guilds = guild;
 module.exports.Status = status;
 module.exports.Rpg = rpg;
 module.exports.Users = user;
-module.exports.Warns = warn;
