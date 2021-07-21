40fd49c7fb27b00e6b16782b193fa407a8b724b2
Luxanna <CommandPlayermine@gmail.com>
2021-07-21 17:38:17 -0300

    feat(Util): Change to ts

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -1,18 +1,23 @@
-const { User } = require('discord.js');
+import { Guild, GuildMember, User } from 'discord.js';
+import MenheraClient from 'MenheraClient';
 
 const MENTION_REGEX = /^(?:<@!?)?([0-9]{16,18})(?:>)?$/;
-module.exports = class Util {
-  static getIdByMention(mention) {
+export default class Util {
+  static getIdByMention(mention: string): string {
     if (!mention) return null;
     const regexResult = MENTION_REGEX.exec(mention);
     return regexResult && regexResult[1];
   }
 
-  static async databaseEnsure(model, query, defaultValue) {
+  static async databaseEnsure(
+    model,
+    query: { id: string },
+    defaultValue: { id: string; shipValue?: number },
+  ) {
     return (await model.findOne(query)) ?? model.create(defaultValue);
   }
 
-  static async databaseUserEnsure(client, userOrMember) {
+  static async databaseUserEnsure(client: MenheraClient, userOrMember: GuildMember | User) {
     const user = userOrMember instanceof User ? userOrMember : userOrMember.user;
     const defaultValue = {
       id: user.id,
@@ -21,12 +26,12 @@ module.exports = class Util {
     return Util.databaseEnsure(client.database.Users, { id: user.id }, defaultValue);
   }
 
-  static async databaseGuildEnsure(client, guild) {
+  static async databaseGuildEnsure(client: MenheraClient, guild: Guild) {
     const { id } = guild;
     return Util.databaseEnsure(client.database.Guilds, { id }, { id });
   }
 
-  static captalize(str) {
+  static captalize(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
-};
+}
