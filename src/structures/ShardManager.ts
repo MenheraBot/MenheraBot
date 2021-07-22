47d52ef5f8be07396def77117100021af9340670
Luxanna <CommandPlayermine@gmail.com>
2021-07-22 18:44:13 -0300

    feat(ShardManager): Change to TS

--- src/structures/ShardManager.ts
+++ src/structures/ShardManager.ts
@@ -1,42 +1,44 @@
-module.exports = class ShardManager {
-  constructor(client) {
+import MenheraClient from 'MenheraClient';
+
+export default class ShardManager {
+  constructor(public client: MenheraClient) {
     this.client = client;
   }
 
-  async getFromCollection(collection, id) {
+  async getFromCollection(collection: string, id: number) {
     const data = await this.client.shard
       .broadcastEval(`this.${collection}.cache.get('${id}')`)
       .then((a) => a.filter((b) => b));
     return data[0];
   }
 
-  async getSizeCollection(collection) {
+  async getSizeCollection(collection: string) {
     const info = await this.client.shard.fetchClientValues(`${collection}.cache.size`);
     const i = info.reduce((prev, val) => prev + val);
     return i;
   }
 
-  getAllSizeObject(collection) {
+  getAllSizeObject(collection: string) {
     return this.getSizeCollection(collection);
   }
 
-  getEmojis(id) {
+  getEmojis(id: number) {
     return this.getFromCollection('emojis', id);
   }
 
-  getUsers(id) {
+  getUsers(id: number) {
     return this.getFromCollection('users', id);
   }
 
-  getGuilds(id) {
+  getGuilds(id: number) {
     return this.getFromCollection('guilds', id);
   }
 
-  getChannels(id) {
+  getChannels(id: number) {
     return this.getFromCollection('channels', id);
   }
 
-  killShard(id) {
+  killShard(id: number) {
     return this.client.shard.broadcastEval(`if (this.shard.id === ${id}) { this.destroy() }`);
   }
-};
+}
