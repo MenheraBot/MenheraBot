a5ad76dd12b15246da1acec818ecc94b8bc5acde
Luxanna <CommandPlayermine@gmail.com>
2021-07-22 20:22:08 -0300

    feat(RpgHandler): Change to TS

---
+++ src/structures/RpgHandler.ts
@@ -0,0 +1,15 @@
+import abilities from './Rpgs/abilities.json';
+import mobs from './Rpgs/mobs.json';
+import items from './Rpgs/items.json';
+import checks from './Rpgs/checks';
+import jobs from './Rpgs/jobs.json';
+import familiars from './Rpgs/familiar.json';
+
+export default {
+  abilities,
+  mobs,
+  items,
+  checks,
+  jobs,
+  familiars,
+};
