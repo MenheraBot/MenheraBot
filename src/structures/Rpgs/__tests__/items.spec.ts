dd52c16bbe30b0caf6305f7af03006713511cb65
Yslan Ramos <yslan3000@gmail.com>
2021-07-29 01:25:03 -0300

    fix(items.spec.ts): fix types

--- src/structures/Rpgs/__tests__/items.spec.ts
+++ src/structures/Rpgs/__tests__/items.spec.ts
@@ -1,21 +1,19 @@
-import { IDungeonMob, IMobLoot } from '@utils/Types';
 import items from '../items.json';
 import mobs from '../mobs.json';
 
 it('should check if the items required by ferreiro exists', () => {
-  const drops = Object.keys(mobs).reduce((p, v) => {
-    const loots = mobs[v]
-      .map((mob: IDungeonMob) => mob.loots)
+  const drops = Object.keys(mobs).reduce<string[]>((p, v) => {
+    const loots = mobs[v as keyof typeof mobs]
+      .map((mob) => mob.loots)
       .flat()
-      .map((loot: IMobLoot) => loot?.name)
-      .filter((loot: IMobLoot) => loot);
+      .map((loot) => loot.name)
+      .filter((loot) => loot);
     return p.concat(loots);
   }, []);
 
-  const nonexistentItems = items.ferreiro.reduce((p, equip) => {
-    const itemsNotFound = Object.keys(equip?.required_items ?? {}).filter(
-      (item) => !drops.includes(item),
-    );
+  const nonexistentItems = items.ferreiro.reduce<string[]>((p, equip) => {
+    const ItemNames = Object.keys(equip?.required_items ?? {});
+    const itemsNotFound = ItemNames.filter((item) => !drops.includes(item));
     return p.concat(itemsNotFound);
   }, []);
 
