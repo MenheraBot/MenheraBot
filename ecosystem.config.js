c76a73a9d622027cee1bea1be1de123a43d2de7c
Luxanna <CommandPlayermine@gmail.com>
2021-08-03 21:49:32 -0300

    fix: pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'Menhera',
-      script: './shard.js',
+      script: './dist/shard.js',
       watch: false,
       max_memory_restart: '4096M',
       interpreter_args: '--max-old-space-size=4096',
