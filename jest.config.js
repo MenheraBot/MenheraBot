96252e96db0a6d9ec27cdb623645e90beecd95bc
Yslan Ramos <yslan3000@gmail.com>
2021-08-04 09:57:39 -0300

    setup ts-jest (#102)
    * feat(Client): Change from js to ts
    
    * feat(FileUtil): Rewrite to TS
    
    * feat(Shard): Rewrite to TS
    
    * fix(FileUtil): Fix some typos
    
    * feat(EventManager): Change to ts
    
    * feat(Util): Change to ts
    
    * feat(NewHttp): Change to ts
    
    * feat(Events): Change to ts
    
    * feat(GuildDelete): Change to ts
    
    * feat(GuildCreate): Change to ts
    
    * feat(GuildCreate): Change to ts
    
    * feat(Message): Make message Event AND Command structure ts
    
    * feat(DBL): Change to ts
    
    * a
    
    * NEED HELP
    
    * feat(RPGUtil): Change to ts
    
    * feat(HTTP): Change to ts
    
    * feat(ShardManager): Change to TS
    
    * feat(CommandContext): Change to TS
    
    * feat(Locale): Change to TS
    
    Update some packages too
    
    * feat(Constants): Change to TS
    
    * feat(ProfileBadge): Change to TS
    
    * feat(RpgHandler): Change to TS
    
    * essa porra ta me deixando irritada que se foda continuo outro dia boa noite
    
    * feat(Database): AI MDS É TAO GOSTOSO QUANDO FUNCIONA!
    
    Finish database system
    CHANGE TO TS
    
    * feat(DatabaseRepositories): Change to TS
    
    * feat(RpgChecks): Change to TS
    
    * feat(ModCommands): Change to TS
    
    * feat(DevCommands): Change to TS
    
    * feat(TS) Varios arquivos perdao tsutsu big commit
    
    * feat: Change to TS
    
    * feat: Change to ts
    
    * feat: Change to TS
    
    * feat: Change to TS
    
    * fix: Fix some lint
    
    * feat(Dockerfile): Better Docker
    
    * fix: Fix type daquilo ali
    
    * ci: check types
    
    * fix: types
    
    * ci: typos
    
    * build: ignore libs
    
    * ci: add cache
    
    * Fix types and CI (#93)
    
    * ci: check types
    
    * fix: types
    
    * ci: typos
    
    * ci: remove ci cache from actions/setup-node
    
    * feat(tsconfig): more strict
    
    * refactor(shard): pretty
    
    * fix(util): types
    
    * fix(rpg-util): types and refactor
    
    * fix(file-util): types
    
    * fix(dbl): types and remove unused code
    
    * fix(command-context): types
    
    * refactor(ready-event): remove repeat code
    
    * feat(guild-create-event): throws an error when id is not provided
    
    * feat(guild-delete-event): throws an error when id is not provided
    
    * fix(ready-event): remove unused shard validation
    
    * refactor(rpg-util): remove unused optional chaining
    
    * fix(items.spec.ts): fix types
    
    * chore: add ts-jest and config file
    
    * chore(jest): add scripts
    
    * chore(jest-config): lint
    
    Co-authored-by: Luxanna <CommandPlayermine@gmail.com>

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+};
