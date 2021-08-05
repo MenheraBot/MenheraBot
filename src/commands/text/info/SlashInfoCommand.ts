7ba2e0a4cf471a03fc6bcc522a76f6468bdf5cb1
Luxanna <CommandPlayermine@gmail.com>
2021-08-05 18:44:33 -0300

    feat: Add slash command to text command to say the state of slas commands

---
+++ src/commands/text/info/SlashInfoCommand.ts
@@ -0,0 +1,59 @@
+import Command from '@structures/Command';
+import 'moment-duration-format';
+import MenheraClient from 'MenheraClient';
+import CommandContext from '@structures/CommandContext';
+import { MessageEmbed } from 'discord.js';
+
+export default class SlashinfoCommand extends Command {
+  constructor(client: MenheraClient) {
+    super(client, {
+      name: 'slashinfo',
+      aliases: ['slash'],
+      cooldown: 10,
+      category: 'info',
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async run(ctx: CommandContext): Promise<void> {
+    const allTextCommands = this.client.commands.size;
+    const allSlashCommands = this.client.slashCommands.size;
+
+    const progress = (allSlashCommands * 100) / allTextCommands;
+
+    const embed = new MessageEmbed()
+      .setTitle('Estado dos Slash Commands')
+      .setColor('BLURPLE')
+      .setDescription(
+        `Ainda faltam **${allTextCommands - allSlashCommands}** comandos para serem transcritos`,
+      )
+      .addFields(
+        [
+          {
+            name: 'Text Commands',
+            value: allTextCommands.toString(),
+            inline: true,
+          },
+          {
+            name: 'Slash Commands',
+            value: allSlashCommands.toString(),
+            inline: true,
+          },
+        ],
+        [
+          {
+            name: 'Slash Criados até o momento',
+            value: `\`${this.client.slashCommands.map((a) => a.config.name).join('`, `')}\``,
+            inline: false,
+          },
+          {
+            name: 'Progresso',
+            value: `**${progress.toFixed(2)}%**`,
+            inline: true,
+          },
+        ],
+      );
+
+    ctx.send(embed);
+  }
+}
