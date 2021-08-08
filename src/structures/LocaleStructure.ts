a2e04f519a21ca5937f2e10558b12aeb8a481daa
Luxanna <CommandPlayermine@gmail.com>
2021-08-08 04:38:15 -0300

    feat: Add untrisal to slash command

--- src/structures/LocaleStructure.ts
+++ src/structures/LocaleStructure.ts
@@ -9,7 +9,7 @@ export default class LocaleStructure {
   public languages: Array<string>;
 
   constructor() {
-    this.ns = ['commands', 'events', 'permissions', 'roleplay'];
+    this.ns = ['common', 'commands', 'events', 'permissions', 'roleplay'];
     this.languages = ['pt-BR', 'en-US'];
   }
 
