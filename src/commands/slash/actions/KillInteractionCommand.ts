7e0c5ab17db0fb921e036dc28f2961134fe528d5
Luxanna <CommandPlayermine@gmail.com>
2021-08-09 00:52:14 -0300

    feat: Add kill command to slash

---
+++ src/commands/slash/actions/KillInteractionCommand.ts
@@ -0,0 +1,82 @@
+import MenheraClient from 'MenheraClient';
+import { COLORS } from '@structures/MenheraConstants';
+import InteractionCommand from '@structures/command/InteractionCommand';
+import InteractionCommandContext from '@structures/command/InteractionContext';
+import { MessageEmbed } from 'discord.js';
+import HttpRequests from '@utils/HTTPrequests';
+
+export default class KillInteractionCommand extends InteractionCommand {
+  constructor(client: MenheraClient) {
+    super(client, {
+      name: 'matar',
+      description: '「☠️」・Mate aquela pessoa que tu não aguenta mais (de mentirinha hihi)',
+      category: 'actions',
+      options: [
+        {
+          type: 'USER',
+          name: 'user',
+          description: 'Usuário que você quer matar',
+          required: true,
+        },
+      ],
+      cooldown: 5,
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async run(ctx: InteractionCommandContext): Promise<void> {
+    const rand = await HttpRequests.getAssetImageUrl('kill');
+    const { user } = ctx.args[0];
+    const avatar = ctx.interaction.user.displayAvatarURL({ format: 'png', dynamic: true });
+
+    if (!user) {
+      await ctx.replyT('error', 'commands:kill.no-mention', {}, true);
+      return;
+    }
+
+    if (user.id === ctx.interaction.user.id) {
+      await ctx.replyT('error', 'commands:kill.self-mention');
+      return;
+    }
+
+    if (user.bot) {
+      // links de robos
+      const ro = [
+        'https://i.imgur.com/tv9wQai.gif',
+        'https://i.imgur.com/X9uUyEB.gif',
+        'https://i.imgur.com/rtsjxWQ.gif',
+      ];
+
+      const Rrand = ro[Math.floor(Math.random() * ro.length)];
+
+      const Rembed = new MessageEmbed()
+        .setTitle(ctx.locale('commands:kill.bot.embed_title'))
+        .setColor(COLORS.ACTIONS)
+        .setDescription(
+          ctx.locale('commands:kill.bot.embed_description', {
+            author: ctx.interaction.user.toString(),
+            mention: user.toString(),
+          }),
+        )
+        .setImage(Rrand)
+        .setThumbnail(avatar);
+
+      await ctx.reply({ embeds: [Rembed] });
+      return;
+    }
+
+    const embed = new MessageEmbed()
+      .setTitle(ctx.locale('commands:kill.embed_title'))
+      .setColor(COLORS.ACTIONS)
+      .setDescription(
+        ctx.locale('commands:kill.embed_description', {
+          author: ctx.interaction.user.toString(),
+          mention: user.toString(),
+        }),
+      )
+      .setImage(rand)
+      .setThumbnail(avatar);
+
+    await ctx.reply({ embeds: [embed] });
+  }
+}
