fcd244e0d4ea5fe7b807f41bc112f9629b1e66dd
Luxanna <CommandPlayermine@gmail.com>
2021-08-09 18:43:16 -0300

    feat: Add pat command to slash

---
+++ src/commands/slash/actions/PatInteractionCommand.ts
@@ -0,0 +1,52 @@
+import MenheraClient from 'MenheraClient';
+import { COLORS } from '@structures/MenheraConstants';
+import InteractionCommand from '@structures/command/InteractionCommand';
+import InteractionCommandContext from '@structures/command/InteractionContext';
+import { MessageEmbed, User } from 'discord.js';
+import HttpRequests from '@utils/HTTPrequests';
+
+export default class CryInteractionCommand extends InteractionCommand {
+  constructor(client: MenheraClient) {
+    super(client, {
+      name: 'carinho',
+      description: '「🥰」・Oti meudeus, faz carinho em alguém',
+      options: [
+        {
+          name: 'user',
+          type: 'USER',
+          description: 'Usuário que você quer fazer carinho',
+          required: true,
+        },
+      ],
+      category: 'actions',
+      cooldown: 5,
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async run(ctx: InteractionCommandContext): Promise<void> {
+    const rand = await HttpRequests.getAssetImageUrl('pat');
+    const user = ctx.args[0].user as User;
+
+    if (user.id === ctx.interaction.user.id) {
+      await ctx.replyT('error', 'commands:pat.self-mention');
+      return;
+    }
+
+    const avatar = ctx.interaction.user.displayAvatarURL({ format: 'png', dynamic: true });
+
+    const embed = new MessageEmbed()
+      .setTitle(ctx.locale('commands:pat.embed_title'))
+      .setColor(COLORS.ACTIONS)
+      .setDescription(
+        ctx.locale('commands:pat.embed_description', {
+          autor: ctx.interaction.user.toString(),
+          mention: user.toString(),
+        }),
+      )
+      .setImage(rand)
+      .setThumbnail(avatar);
+
+    await ctx.reply({ embeds: [embed] });
+  }
+}
