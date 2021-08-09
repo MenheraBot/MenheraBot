31d092240932c0ea56303cc583c3e4472f79b692
Luxanna <CommandPlayermine@gmail.com>
2021-08-09 18:27:43 -0300

    feat: Kiss command to slash

---
+++ src/commands/slash/actions/KissInteractionCommand.ts
@@ -0,0 +1,57 @@
+import MenheraClient from 'MenheraClient';
+import { COLORS } from '@structures/MenheraConstants';
+import InteractionCommand from '@structures/command/InteractionCommand';
+import InteractionCommandContext from '@structures/command/InteractionContext';
+import { MessageEmbed, User } from 'discord.js';
+import HttpRequests from '@utils/HTTPrequests';
+
+export default class KissInteractionCommand extends InteractionCommand {
+  constructor(client: MenheraClient) {
+    super(client, {
+      name: 'beijar',
+      description: '「😘」・De uma beijoquita em alguém que tu goste',
+      category: 'actions',
+      options: [
+        {
+          type: 'USER',
+          name: 'user',
+          description: 'Usuário que você quer beijar',
+          required: true,
+        },
+      ],
+      cooldown: 5,
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async run(ctx: InteractionCommandContext): Promise<void> {
+    const rand = await HttpRequests.getAssetImageUrl('kiss');
+    const user = ctx.args[0].user as User;
+
+    if (user.bot) {
+      await ctx.replyT('error', 'commands:kiss.bot');
+      return;
+    }
+
+    if (user.id === ctx.interaction.user.id) {
+      await ctx.replyT('error', 'commands:kiss.self-mention', {}, true);
+      return;
+    }
+
+    const avatar = ctx.interaction.user.displayAvatarURL({ format: 'png', dynamic: true });
+
+    const embed = new MessageEmbed()
+      .setTitle(ctx.locale('commands:kiss.embed_title'))
+      .setColor(COLORS.ACTIONS)
+      .setDescription(
+        ctx.locale('commands:kiss.embed_description', {
+          author: ctx.interaction.user.toString(),
+          mention: user.toString(),
+        }),
+      )
+      .setImage(rand)
+      .setThumbnail(avatar);
+
+    await ctx.reply({ embeds: [embed] });
+  }
+}
