0ffa7f96d4981a1676236490f4d67414d34988c4
Luxanna <CommandPlayermine@gmail.com>
2021-08-08 23:54:42 -0300

    feat: Bite command to slash

---
+++ src/commands/slash/actions/BiteInteractionCommand.ts
@@ -0,0 +1,62 @@
+import MenheraClient from 'MenheraClient';
+import { COLORS } from '@structures/MenheraConstants';
+import InteractionCommand from '@structures/command/InteractionCommand';
+import InteractionCommandContext from '@structures/command/InteractionContext';
+import { MessageEmbed } from 'discord.js';
+import HttpRequests from '@utils/HTTPrequests';
+
+export default class BiteInteractionCommand extends InteractionCommand {
+  constructor(client: MenheraClient) {
+    super(client, {
+      name: 'morder',
+      description: '「👄」・Nhac. Moide alguém >.<',
+      options: [
+        {
+          name: 'user',
+          type: 'USER',
+          description: 'Usuário para morder :3',
+          required: true,
+        },
+      ],
+      category: 'actions',
+      cooldown: 5,
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async run(ctx: InteractionCommandContext): Promise<void> {
+    const rand = await HttpRequests.getAssetImageUrl('bite');
+    const { user } = ctx.args[0];
+
+    if (!user) {
+      await ctx.replyT('error', 'commands:bite.no-mention', {}, true);
+      return;
+    }
+
+    if (user.bot) {
+      await ctx.replyT('warn', 'commands:bite.bot');
+      return;
+    }
+
+    if (user.id === ctx.interaction.user.id) {
+      await ctx.replyT('error', 'commands:bite.self-mention', {}, true);
+      return;
+    }
+
+    const avatar = ctx.interaction.user.displayAvatarURL({ format: 'png', dynamic: true });
+
+    const embed = new MessageEmbed()
+      .setTitle(ctx.locale('commands:bite.embed_title'))
+      .setColor(COLORS.ACTIONS)
+      .setDescription(
+        ctx.locale('commands:bite.embed_description', {
+          author: ctx.interaction.user.toString(),
+          mention: user.toString(),
+        }),
+      )
+      .setImage(rand)
+      .setThumbnail(avatar);
+
+    await ctx.reply({ embeds: [embed] });
+  }
+}
