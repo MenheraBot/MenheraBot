8732ff969059718fefff2f2b60e33ac896db5bfc
Luxanna <CommandPlayermine@gmail.com>
2021-08-11 01:07:27 -0300

    feat: Add support command to interaction

---
+++ src/commands/slash/util/SupportInteractionCommand.ts
@@ -0,0 +1,26 @@
+import MenheraClient from 'MenheraClient';
+import InteractionCommand from '@structures/command/InteractionCommand';
+import InteractionCommandContext from '@structures/command/InteractionContext';
+import { MessageEmbed } from 'discord.js';
+
+export default class SupportInteractionCommand extends InteractionCommand {
+  constructor(client: MenheraClient) {
+    super(client, {
+      name: 'suporte',
+      description: '「🙋」・Precisa de ajuda? Entre no meu servidor de suporte',
+      category: 'util',
+      cooldown: 5,
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async run(ctx: InteractionCommandContext): Promise<void> {
+    const embed = new MessageEmbed()
+      .setTitle(ctx.locale('commands:support.embed_title'))
+      .setURL('https://discord.gg/fZMdQbA')
+      .setColor('#970045')
+      .setImage('https://i.imgur.com/ZsKuh8W.png')
+      .setTimestamp();
+    await ctx.reply({ embeds: [embed], ephemeral: true });
+  }
+}
