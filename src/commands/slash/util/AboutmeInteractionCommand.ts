35108466139dffba9aa32f6f726f53895549b70e
Luxanna <CommandPlayermine@gmail.com>
2021-08-10 23:12:37 -0300

    feat: Add aboutme command to slash

---
+++ src/commands/slash/util/AboutmeInteractionCommand.ts
@@ -0,0 +1,36 @@
+import MenheraClient from 'MenheraClient';
+import InteractionCommand from '@structures/command/InteractionCommand';
+import InteractionCommandContext from '@structures/command/InteractionContext';
+
+export default class AboutmeInteractionCommand extends InteractionCommand {
+  constructor(client: MenheraClient) {
+    super(client, {
+      name: 'sobremim',
+      description: '「💬」・Mude o seu sobremim (A mensagem que aparece em seu perfil)',
+      category: 'util',
+      options: [
+        {
+          type: 'STRING',
+          name: 'frase',
+          description: 'Frase para colocar em seu sobre mim. No máximo 200 caracteres',
+          required: true,
+        },
+      ],
+      cooldown: 5,
+      clientPermissions: ['EMBED_LINKS'],
+    });
+  }
+
+  async run(ctx: InteractionCommandContext): Promise<void> {
+    const nota = ctx.options.getString('frase', true);
+
+    if (nota.length > 200) {
+      await ctx.replyT('error', 'commands:aboutme.args-limit', {}, true);
+      return;
+    }
+
+    await ctx.client.repositories.userRepository.update(ctx.interaction.user.id, { nota });
+
+    await ctx.replyT('success', 'commands:aboutme.success');
+  }
+}
