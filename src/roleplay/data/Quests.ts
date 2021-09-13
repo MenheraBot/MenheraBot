d37b4ff5354bb1b5ba645f989d8ff3415756e5b6
Luxanna <CommandPlayermine@gmail.com>
2021-09-13 16:09:26 -0300

    feat(Quest): Create first quest

--- src/roleplay/data/Quests.ts
+++ src/roleplay/data/Quests.ts
@@ -1,5 +1,31 @@
 import { IQuestsFile } from '@roleplay/Types';
 
-const quests: { [key: number]: IQuestsFile } = {};
+const quests: { [key: number]: IQuestsFile } = {
+  0: {
+    description: 'Matar goblin da guilda',
+    isDaily: false,
+    minUserLevel: 1,
+    objective: {
+      amount: 2,
+      perLevel: 0,
+      type: 'kill_enemy',
+      value: 0,
+    },
+    reward: {
+      experience: 20,
+      perLevel: {
+        bronze: 1,
+        gold: 0,
+        silver: 0,
+      },
+      type: 'money',
+      amount: {
+        bronze: 3,
+        gold: 0,
+        silver: 0,
+      },
+    },
+  },
+};
 
 export default quests;
