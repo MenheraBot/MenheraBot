ddf0bff866520dbdf2db65e0303319e5f8587391
Luxanna <CommandPlayermine@gmail.com>
2021-09-23 12:32:13 -0300

    feat(WebSocket): Implement reconnection system

--- src/structures/PicassoWebSocket.ts
+++ src/structures/PicassoWebSocket.ts
@@ -3,6 +3,8 @@ import WebSocket from 'ws';
 export default class PicassoWebSocket {
   public isAlive = false;
 
+  private pingTimeout: ReturnType<typeof setTimeout> | undefined;
+
   private ws: WebSocket | null = null;
 
   async connect(): Promise<void> {
@@ -10,26 +12,39 @@ export default class PicassoWebSocket {
       this.ws = new WebSocket(`${process.env.PICASSO_WEBSOCKET}`);
       this.prepareListeners();
     } catch (err) {
-      if (err instanceof Error) console.log('[WEBSOCKET] Error when connecting: %s', err.message);
+      if (err instanceof Error) console.log(`[WEBSOCKET] Error when connecting: ${err.message}`);
     }
+  }
+
+  private heartbeat(): void {
+    if (typeof this.pingTimeout === 'undefined') return;
+    clearTimeout(this.pingTimeout);
+
+    this.isAlive = true;
+    this.ws?.pong(16);
+
+    this.pingTimeout = setTimeout(() => {
+      this.ws?.terminate();
+    }, 15000);
+  }
+
+  private onClose(): void {
+    if (!this.ws) return;
+
+    this.ws.terminate();
+    if (this.pingTimeout) clearTimeout(this.pingTimeout);
+
+    setTimeout(() => {
+      if (!this.ws) return;
+      this.ws.removeAllListeners();
+      this.connect();
+    }, 5000);
   }
 
   private prepareListeners(): void {
     if (!this.ws) return;
 
-    this.ws.once('open', () => {
-      this.isAlive = true;
-      console.log('abriu');
-    });
-
-    this.ws.once('close', () => {
-      this.isAlive = false;
-      console.log('a');
-    });
-
-    this.ws.on('ping', () => {
-      this.isAlive = true;
-    });
+    this.ws.once('open', this.heartbeat).once('close', this.onClose).on('ping', this.heartbeat);
 
     //   this.ws.on('message', (msg: Buffer) => this.handleData(JSON.parse(msg.toString())));
   }
