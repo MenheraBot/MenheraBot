d24b128e787d92bfc7bb53689121f23818e098a4
Luxanna <CommandPlayermine@gmail.com>
2021-09-27 10:30:32 -0300

    Feat: Finish http interactions system

--- src/structures/server/controllers/PostInteractions.ts
+++ src/structures/server/controllers/PostInteractions.ts
@@ -1,17 +1,32 @@
+/* eslint-disable no-bitwise */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Context } from 'koa';
 import Router from 'koa-router';
 import MenheraClient from 'MenheraClient';
+import { Client } from 'discord.js-light';
 import authenticateDsicordRequests from '../middlewares/authenticateDsicordRequests';
 
-const handleRequest = (ctx: Context, client: MenheraClient) => {
+const handleRequest = async (ctx: Context, client: MenheraClient) => {
   if (ctx.request.body.type === 1) {
     ctx.status = 200;
     ctx.body = { type: 1 };
     return;
   }
-  // @ts-ignore
-  client.actions.InteractionCreate.handle(ctx.request.body);
+
+  const shardToExecute = Number(ctx.request.body.guildId) >> 22 % (client.shard?.count as number);
+  await client.shard
+    ?.broadcastEval(
+      (c: Client, { data }: { data: unknown }) => {
+        if (!c.isReady()) return;
+        // @ts-ignore
+        c.actions.InteractionCreate.handle(data);
+      },
+      {
+        shard: shardToExecute,
+        context: { data: ctx.request.body },
+      },
+    )
+    .catch(() => null);
 };
 
 export default (client: MenheraClient): Router => {
