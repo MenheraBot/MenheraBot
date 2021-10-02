1aff86a882c06e8532fe9da3f972dd0b013ac1c4
Luxanna <CommandPlayermine@gmail.com>
2021-10-02 01:59:35 -0300

    Fix: Add id to mobAttacks

--- src/roleplay/data/MobAttacks.ts
+++ src/roleplay/data/MobAttacks.ts
@@ -2,6 +2,7 @@ import { IMobAttacksFile } from '@roleplay/Types';
 
 const attacks: { [key: number]: IMobAttacksFile } = {
   0: {
+    id: 0,
     description: 'O bicho da um ataque com uma faquinha',
     element: 'fire',
     effects: [
