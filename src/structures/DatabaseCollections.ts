9f4ab6cebb4a81bdc9dee741bfe4fd18fc03744e
ySnoopyDogy <CommandPlayermine@gmail.com>
2021-10-26 23:53:32 -0300

    refactor(DatabaseLabels): Add new database labels

--- src/structures/DatabaseCollections.ts
+++ src/structures/DatabaseCollections.ts
@@ -16,25 +16,38 @@ const guildSchema = new Schema({
 
 const userSchema = new Schema({
   id: { type: String, unique: true, index: true },
-  mamadas: { type: Number, default: 0 },
+  mamado: { type: Number },
   mamou: { type: Number, default: 0 },
-  casado: { type: String, default: 'false' },
-  nota: { type: String, default: 'Eu amo a Menhera >.<\nUse /sobremim!' },
-  data: { type: String, default: undefined },
+  mamadas: { type: Number, default: 0 }, // Rename to mamado
+  casado: { type: String, default: 'false' }, // Rename to married
+  married: { type: String },
+  info: { type: String },
+  nota: { type: String, default: 'Eu amo a Menhera >.<\nUse /sobremim!' }, // Rename to info
+  data: { type: String, default: undefined }, // Rename to marriedData
+  marriedData: { type: String },
   ban: { type: Boolean, default: false },
   banReason: { type: String, default: null },
-  cor: { type: String, default: '#a788ff' },
-  cores: { type: Array, default: [{ nome: '0 - Padrão', cor: '#a788ff', preço: 0 }] },
-  caçados: { type: Number, default: 0 },
+  color: { type: String },
+  cor: { type: String, default: '#a788ff' }, // Rename to color
+  cores: { type: Array, default: [{ nome: '0 - Padrão', cor: '#a788ff', preço: 0 }] }, // Rename to colors
+  colors: { type: Array },
+  caçados: { type: Number, default: 0 }, // Rename to demons
+  demons: { type: Number },
   giants: { type: Number, default: 0 },
-  anjos: { type: Number, default: 0 },
-  semideuses: { type: Number, default: 0 },
-  arcanjos: { type: Number, default: 0 },
-  deuses: { type: Number, default: 0 },
-  caçarTime: { type: String, default: '000000000000' },
+  angels: { type: Number },
+  anjos: { type: Number, default: 0 }, // Rename to angels
+  demigods: { type: Number },
+  semideuses: { type: Number, default: 0 }, // Rename to demigods
+  archangels: { type: Number },
+  arcanjos: { type: Number, default: 0 }, // Rename to archangels
+  gods: { type: Number },
+  deuses: { type: Number, default: 0 }, // Rename to gods
+  caçarTime: { type: String, default: '000000000000' }, // Rename to huntCooldown
+  huntCooldown: { type: String },
   rolls: { type: Number, default: 0 },
   estrelinhas: { type: Number, default: 0 },
-  votos: { type: Number, default: 0 },
+  votos: { type: Number, default: 0 }, // Rename to votes
+  votes: { type: Number },
   badges: { type: Array, default: [] },
   voteCooldown: { type: String, default: '000000000000' },
   trisal: { type: Array, default: [] },
