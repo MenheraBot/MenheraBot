986a96e7941f3518583b8564895e6361213bd2a1
Luxanna <CommandPlayermine@gmail.com>
2021-11-05 01:16:49 -0300

    feat: Create Magic Items

--- src/structures/MagicItems.ts
+++ src/structures/MagicItems.ts
@@ -5,15 +5,66 @@ const MagicItems: { [id: number]: TMagicItemsFile<HuntingTypes> } = {
     type: 'PROBABILITY_BOOST',
     huntType: 'demons',
     probabilities: [
-      { amount: 1, probabilty: 20 },
-      { amount: 2, probabilty: 20 },
-      { amount: 3, probabilty: 30 },
+      { amount: 1, probabilty: 46 },
+      { amount: 2, probabilty: 18 },
       { amount: 4, probabilty: 15 },
-      { amount: 5, probabilty: 8 },
-      { amount: 6, probabilty: 5 },
-      { amount: 0, probabilty: 2 },
+      { amount: 3, probabilty: 12 },
+      { amount: 5, probabilty: 9 },
     ],
-    cost: 800000,
+    cost: 450_000,
+  },
+  2: {
+    type: 'PROBABILITY_BOOST',
+    huntType: 'giants',
+    probabilities: [
+      { amount: 1, probabilty: 46 },
+      { amount: 2, probabilty: 19 },
+      { amount: 4, probabilty: 18 },
+      { amount: 0, probabilty: 10 },
+      { amount: 3, probabilty: 7 },
+    ],
+    cost: 600_000,
+  },
+  3: {
+    type: 'PROBABILITY_BOOST',
+    huntType: 'angels',
+    probabilities: [
+      { amount: 1, probabilty: 60 },
+      { amount: 0, probabilty: 20 },
+      { amount: 2, probabilty: 15 },
+      { amount: 3, probabilty: 5 },
+    ],
+    cost: 780_000,
+  },
+  4: {
+    type: 'PROBABILITY_BOOST',
+    huntType: 'archangels',
+    probabilities: [
+      { amount: 1, probabilty: 47 },
+      { amount: 0, probabilty: 34 },
+      { amount: 2, probabilty: 15 },
+      { amount: 3, probabilty: 4 },
+    ],
+    cost: 900_000,
+  },
+  5: {
+    type: 'PROBABILITY_BOOST',
+    huntType: 'demigods',
+    probabilities: [
+      { amount: 1, probabilty: 55 },
+      { amount: 0, probabilty: 40 },
+      { amount: 2, probabilty: 5 },
+    ],
+    cost: 1_250_000,
+  },
+  6: {
+    type: 'PROBABILITY_BOOST',
+    huntType: 'gods',
+    probabilities: [
+      { amount: 0, probabilty: 75 },
+      { amount: 1, probabilty: 25 },
+    ],
+    cost: 1_500_000,
   },
 };
 
