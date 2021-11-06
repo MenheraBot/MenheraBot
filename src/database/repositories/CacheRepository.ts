2eb70bce7b262328040bbbb28459f91ed1bca4ca
ySnoopyDogy <CommandPlayermine@gmail.com>
2021-11-05 21:29:12 -0300

    feat: Make Menhera Redis Problem

--- src/database/repositories/CacheRepository.ts
+++ src/database/repositories/CacheRepository.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-shadow */
 import { ICmdSchema, IGuildSchema } from '@utils/Types';
+import { debugError } from '@utils/Util';
 import { Redis } from 'ioredis';
 import { Document } from 'mongoose';
 import CmdRepository from './CmdsRepository';
@@ -14,22 +15,24 @@ export default class CacheRepository {
 
   async fetchCommand(commandName: string): Promise<ICmdSchema | (ICmdSchema & Document) | null> {
     if (this.redisClient) {
-      const commandData = await this.redisClient.get(`command:${commandName}`);
+      const commandData = await this.redisClient.get(`command:${commandName}`).catch(debugError);
       if (commandData) return JSON.parse(commandData);
     }
+
     const commandDataFromMongo = await this.cmdRepository.findByName(commandName);
     if (!commandDataFromMongo) return null;
 
-    if (this.redisClient) {
-      await this.redisClient.setex(
-        `command:${commandName}`,
-        3600,
-        JSON.stringify({
-          maintenance: commandDataFromMongo.maintenance,
-          maintenanceReason: commandDataFromMongo.maintenanceReason,
-        }),
-      );
-    }
+    if (this.redisClient)
+      await this.redisClient
+        .setex(
+          `command:${commandName}`,
+          3600,
+          JSON.stringify({
+            maintenance: commandDataFromMongo.maintenance,
+            maintenanceReason: commandDataFromMongo.maintenanceReason,
+          }),
+        )
+        .catch(debugError);
 
     return commandDataFromMongo;
   }
@@ -39,21 +42,24 @@ export default class CacheRepository {
     preferredLocale: string,
   ): Promise<IGuildSchema | (IGuildSchema & Document)> {
     if (this.redisClient) {
-      const guildData = await this.redisClient.get(`guild:${guildID}`);
+      const guildData = await this.redisClient.get(`guild:${guildID}`).catch(debugError);
       if (guildData) return JSON.parse(guildData);
     }
+
     const guildDataFromMongo = await this.guildRepository.findOrCreate(guildID, preferredLocale);
 
     if (this.redisClient)
-      await this.redisClient.setex(
-        `guild:${guildID}`,
-        3600,
-        JSON.stringify({
-          lang: guildDataFromMongo.lang,
-          blockedChannels: guildDataFromMongo.blockedChannels,
-          disabledCommands: guildDataFromMongo.disabledCommands,
-        }),
-      );
+      await this.redisClient
+        .setex(
+          `guild:${guildID}`,
+          3600,
+          JSON.stringify({
+            lang: guildDataFromMongo.lang,
+            blockedChannels: guildDataFromMongo.blockedChannels,
+            disabledCommands: guildDataFromMongo.disabledCommands,
+          }),
+        )
+        .catch(debugError);
 
     return guildDataFromMongo;
   }
@@ -61,7 +67,7 @@ export default class CacheRepository {
   async updateGuild(guildID: string, update: IGuildSchema): Promise<void> {
     if (this.redisClient) {
       const stringedObject = JSON.stringify(update);
-      await this.redisClient.setex(`guild:${guildID}`, 3600, stringedObject);
+      await this.redisClient.setex(`guild:${guildID}`, 3600, stringedObject).catch(debugError);
     }
     await this.guildRepository.update(guildID, update);
   }
@@ -69,17 +75,22 @@ export default class CacheRepository {
   async updateCommand(commandName: string, update: ICmdSchema): Promise<void> {
     if (this.redisClient) {
       const stringedObject = JSON.stringify(update);
-      await this.redisClient.setex(`command:${commandName}`, 3600, stringedObject);
+      await this.redisClient
+        .setex(`command:${commandName}`, 3600, stringedObject)
+        .catch(debugError);
     }
   }
 
   async addDeletedAccount(user: string[] | string): Promise<void> {
     if (!this.redisClient) return;
-    await this.redisClient.sadd('deleted_accounts', user);
+    await this.redisClient.sadd('deleted_accounts', user).catch(debugError);
   }
 
   async getDeletedAccounts(): Promise<string[]> {
     if (!this.redisClient) return [];
-    return this.redisClient.smembers('deleted_accounts');
+    return this.redisClient.smembers('deleted_accounts').catch((err) => {
+      debugError(err);
+      return [];
+    });
   }
 }
