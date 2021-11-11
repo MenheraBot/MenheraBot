01d79a4ae11f0dd67cbb201253b85edf51f7a7a6
Lux <59155752+ySnoopyDogy@users.noreply.github.com>
2021-11-10 22:34:15 -0300

    Update MamarRepository.ts

--- src/database/repositories/MamarRepository.ts
+++ src/database/repositories/MamarRepository.ts
@@ -5,6 +5,6 @@ export default class MamarRepository {
 
   async mamar(fromUserID: string, toUserID: string): Promise<void> {
     await this.userRepository.update(fromUserID, { $inc: { mamou: 1 }, lastCommandAt: Date.now() });
-    await this.userRepository.update(toUserID, { $inc: { mamadas: 1 } });
+    await this.userRepository.update(toUserID, { $inc: { mamado: 1 } });
   }
 }
