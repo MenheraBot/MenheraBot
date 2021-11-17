2759a19fd97f159b7c10e2e5292128691ee4e084
Luxanna <CommandPlayermine@gmail.com>
2021-11-17 20:02:11 -0300

    test: permissions

--- src/commands/fun/StealInteractionCommand.ts
+++ src/commands/fun/StealInteractionCommand.ts
@@ -5,7 +5,7 @@ import InteractionCommand from '@structures/command/InteractionCommand';
 import InteractionCommandContext from '@structures/command/InteractionContext';
 import { MessageEmbed } from 'discord.js-light';
 import { HuntingTypes } from '@utils/Types';
-import { getUserStealCooldown } from '@utils/HuntUtils';
+// import { getUserStealCooldown } from '@utils/HuntUtils';
 import Util from '@utils/Util';
 
 const choices: { name: string; value: HuntingTypes }[] = [
@@ -62,7 +62,7 @@ export default class StealInteractionCommand extends InteractionCommand {
   }
 
   async run(ctx: InteractionCommandContext): Promise<void> {
-    const huntType = ctx.options.getString('tipo', true) as HuntingTypes;
+    //   const huntType = ctx.options.getString('tipo', true) as HuntingTypes;
     const user = ctx.options.getUser('user', true);
 
     const canSteal = ctx.data.user.stealCooldown < Date.now();
@@ -79,7 +79,7 @@ export default class StealInteractionCommand extends InteractionCommand {
 
     const avatar = ctx.author.displayAvatarURL({ format: 'png', dynamic: true });
 
-    const cooldown = getUserStealCooldown(ctx.data.user.inUseItems, huntType) + Date.now();
+    // const cooldown = getUserStealCooldown(ctx.data.user.inUseItems, huntType) + Date.now();
 
     const embed = new MessageEmbed()
       .setColor(COLORS.HuntDefault)
@@ -97,14 +97,14 @@ export default class StealInteractionCommand extends InteractionCommand {
     // @ts-expect-error HuntString is actually HuntHUNTYPE
     embed.setColor(COLORS[`Hunt${Util.capitalize(selected)}`]);
 
-    const APIHuntTypes = {
+    /*   const APIHuntTypes = {
       demons: 'demon',
       giants: 'giant',
       angels: 'angel',
       archangels: 'archangel',
       demigods: 'demigod',
       gods: 'god',
-    };
+    }; */
 
     //  HttpRequests.postHuntCommand(ctx.author.id, APIHuntTypes[selected], result);
 
