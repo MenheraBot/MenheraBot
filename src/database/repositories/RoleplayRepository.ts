86a4e7b17d14ebe6055e7209500d474cbb78a598
Luxanna <CommandPlayermine@gmail.com>
2022-02-04 23:18:48 -0300

    feat: Remove redis from rpg for testing

--- src/database/repositories/RoleplayRepository.ts
+++ src/database/repositories/RoleplayRepository.ts
@@ -1,11 +1,11 @@
 import { RoleplayUserSchema } from '@roleplay/Types';
 import { Rpgs } from '@structures/DatabaseCollections';
-import { debugError, MayNotExists } from '@utils/Util';
+import { MayNotExists } from '@utils/Util';
 import { Redis } from 'ioredis';
 import { UpdateQuery, UpdateWithAggregationPipeline } from 'mongoose';
 
 export default class RoleplayRepository {
-  constructor(private roleplayModal: typeof Rpgs, private redisClient: MayNotExists<Redis>) {}
+  constructor(private roleplayModal: typeof Rpgs, public redisClient: MayNotExists<Redis>) {}
 
   async registerUser(
     userId: string,
@@ -21,19 +21,19 @@ export default class RoleplayRepository {
   }
 
   async findUser(userId: string): Promise<MayNotExists<RoleplayUserSchema>> {
-    if (this.redisClient) {
+    /*   if (this.redisClient) {
       const userData = await this.redisClient
         .get(`roleplay:${userId}`)
         .catch((e) => debugError(e, true));
       if (userData) return JSON.parse(userData);
     }
-
+ */
     const fromDatabase = await this.roleplayModal.findOne({ id: userId });
 
-    if (fromDatabase && this.redisClient) {
+    /*  if (fromDatabase && this.redisClient) {
       await this.redisClient.setex(`roleplay:${userId}`, 3600, JSON.stringify(fromDatabase));
     }
-
+ */
     return fromDatabase;
   }
 
@@ -41,20 +41,20 @@ export default class RoleplayRepository {
     userId: string,
     toUpdate: UpdateQuery<RoleplayUserSchema> | UpdateWithAggregationPipeline,
   ): Promise<void> {
-    const updated = await this.roleplayModal.updateOne({ id: userId }, toUpdate, {
+    /*  const updated = */ await this.roleplayModal.updateOne({ id: userId }, toUpdate, {
       returnOriginal: false,
     });
 
-    if (this.redisClient)
-      await this.redisClient.setex(`roleplay:${userId}`, 3600, JSON.stringify(updated));
+    /*   if (this.redisClient)
+      await this.redisClient.setex(`roleplay:${userId}`, 3600, JSON.stringify(updated)); */
   }
 
   async postBattle(userId: string, updatedUserState: RoleplayUserSchema): Promise<void> {
-    const updated = await this.roleplayModal.updateOne({ id: userId }, updatedUserState, {
+    /*   const updated =  */ await this.roleplayModal.updateOne({ id: userId }, updatedUserState, {
       returnOriginal: false,
     });
 
-    if (this.redisClient)
-      await this.redisClient.setex(`roleplay:${userId}`, 3600, JSON.stringify(updated));
+    /*   if (this.redisClient)
+      await this.redisClient.setex(`roleplay:${userId}`, 3600, JSON.stringify(updated)); */
   }
 }
