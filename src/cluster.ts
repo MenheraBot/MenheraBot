63435d3f4b6121f08acd054492faf0ae9af9ea60
Luxanna <CommandPlayermine@gmail.com>
2022-03-07 08:35:23 -0300

    fix: Fix shard count

--- src/cluster.ts
+++ src/cluster.ts
@@ -6,13 +6,13 @@ import { Manager } from 'discord-hybrid-sharding';
 
 console.log('[APP] Initiating application...');
 
-const SHARD_COUNT = process.env.NODE_ENV === 'development' ? 20 : 'auto';
+const SHARD_COUNT = process.env.NODE_ENV === 'development' ? 1 : 'auto';
 const DISCORD_TOKEN =
   process.env.NODE_ENV === 'development' ? process.env.DEV_BOT_TOKEN : process.env.BOT_TOKEN;
 
 const ClusterManager = new Manager(join(__dirname, './index.js'), {
   totalShards: SHARD_COUNT,
-  totalClusters: 4,
+  totalClusters: 'auto',
   token: DISCORD_TOKEN,
   mode: 'worker',
   keepAlive: {
