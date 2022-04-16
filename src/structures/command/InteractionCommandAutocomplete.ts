7cc22e220b76e413b184f78db8ccb2c23d35c550
Luxanna <CommandPlayermine@gmail.com>
2022-04-16 03:36:32 -0300

    fix: Add autocomplete to badges

--- src/structures/command/InteractionCommandAutocomplete.ts
+++ src/structures/command/InteractionCommandAutocomplete.ts
@@ -1,5 +1,6 @@
 import { AutocompleteInteraction } from 'discord.js-light';
 import MenheraClient from 'MenheraClient';
+import GiveBadgeAutocomplete from './autocomplete/GiveBadgesAutocomplete';
 import LibraryAutocomplete from './autocomplete/LibraryAutocomplete';
 
 const ExecuteAutocompleteInteractions = async (
@@ -10,6 +11,8 @@ const ExecuteAutocompleteInteractions = async (
   switch (commandName) {
     case 'centro':
       return LibraryAutocomplete(interaction);
+    case 'givebadge':
+      return GiveBadgeAutocomplete(interaction);
   }
 };
 
