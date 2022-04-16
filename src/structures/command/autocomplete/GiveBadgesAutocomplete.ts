bf683875443cc72d87bbfd7cba78a9b8413ed068
Luxanna <CommandPlayermine@gmail.com>
2022-04-16 03:57:26 -0300

    feat: Best options

--- src/structures/command/autocomplete/GiveBadgesAutocomplete.ts
+++ src/structures/command/autocomplete/GiveBadgesAutocomplete.ts
@@ -10,27 +10,36 @@ const GiveBadgeAutocomplete = async (
 
   if (Date.now() - interaction.createdTimestamp >= 3000) return;
 
-  const badges = Object.entries(Badges).map((a) => ({
+  const entries = Object.entries(Badges);
+
+  const badges: { id: string; name: string }[] = entries.map((a) => ({
     id: a[0],
-    ...a[1],
     name: `${a[0]} ${a[1].name}`,
   }));
+
+  badges.push(...entries.map((c) => ({ name: c[0], id: c[0] })));
 
   const ratings = findBestMatch(
     `${texted}`,
     badges.map((a) => a.name),
   );
 
-  const toSendOptions = ratings.ratings.filter((a) => a.rating >= 0.35);
+  const toSendOptions = ratings.ratings.filter((a) => a.rating >= 0.2);
+
   if (toSendOptions.length === 0) return interaction.respond([]);
 
   const toSendBadges: ApplicationCommandOptionChoice[] = [];
 
   toSendOptions.forEach((a) => {
     if (toSendBadges.length >= 25) return;
-    const badge = badges.find((b) => b.name === a.target);
+    const badge =
+      a.target.length < 3
+        ? badges.find((b) => b.name.startsWith(a.target))
+        : badges.find((b) => b.name === a.target);
+
     if (!badge) return;
-    const toPush = { name: badge.name, value: badge.id };
+
+    const toPush = { name: badge.name, value: Number(badge.id) };
     if (toSendBadges.some((b) => b.value === toPush.value)) return;
     toSendBadges.push(toPush);
   });
