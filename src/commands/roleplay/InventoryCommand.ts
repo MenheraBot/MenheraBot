388f6cab035fef5c28dcb1fecf05d198a4a5cbb4
Luxanna <CommandPlayermine@gmail.com>
2022-06-06 00:50:05 -0300

    feat: finish new inventory command

--- src/commands/roleplay/InventoryCommand.ts
+++ src/commands/roleplay/InventoryCommand.ts
@@ -1,5 +1,9 @@
 import { ConsumableItem, EquipmentItem, ItemsFile } from '@roleplay/Types';
-import { makeCloseCommandButton, removeFromInventory } from '@roleplay/utils/AdventureUtils';
+import {
+  makeCloseCommandButton,
+  removeFromInventory,
+  addToInventory,
+} from '@roleplay/utils/AdventureUtils';
 import { getUserMaxLife, getUserMaxMana } from '@roleplay/utils/Calculations';
 import { getEquipmentById, getItemById } from '@roleplay/utils/DataUtils';
 import InteractionCommand from '@structures/command/InteractionCommand';
@@ -7,6 +11,7 @@ import InteractionCommandContext from '@structures/command/InteractionContext';
 import { IReturnData } from '@custom_types/Menhera';
 import Util, {
   actionRow,
+  debugError,
   disableComponents,
   makeCustomId,
   resolveCustomId,
@@ -16,7 +21,9 @@ import {
   MessageButton,
   MessageEmbed,
   MessageSelectMenu,
+  Modal,
   SelectMenuInteraction,
+  TextInputComponent,
 } from 'discord.js-light';
 import { isItemEquipment } from '@roleplay/utils/ItemsUtil';
 
@@ -75,7 +82,7 @@ export default class InventoryCommand extends InteractionCommand {
       text.length > 0 ? text.join('\n') : ctx.locale('commands:inventario.no-items'),
     );
 
-    const [useCustomId, baseId] = makeCustomId('USE');
+    const [useCustomId, baseId] = makeCustomId('POTION');
     const [equipCustomId] = makeCustomId('EQUIP', baseId);
 
     const usePotion = new MessageButton()
@@ -109,7 +116,7 @@ export default class InventoryCommand extends InteractionCommand {
       ctx.channel,
       ctx.author.id,
       baseId,
-      7000,
+      10_000,
     );
 
     if (!selected) {
@@ -121,97 +128,258 @@ export default class InventoryCommand extends InteractionCommand {
       return;
     }
 
-    // TODO: Diferenciar se é uma poção ou um equip
+    if (resolveCustomId(selected.customId) === 'POTION') {
+      embed.setDescription('');
+
+      const availablePotions = resolvedItems.filter((a) => a.data.type === 'potion') as Array<
+        IReturnData<ConsumableItem> & { level: number; amount: number }
+      >;
+
+      const selectMenu = new MessageSelectMenu().setCustomId(`${ctx.interaction.id} | SELECT`);
+
+      availablePotions.forEach((a) => {
+        const item = getItemById<ConsumableItem>(a.id);
+        embed.addField(
+          ctx.locale(`items:${a.id as 1}.name`),
+          ctx.locale('commands:inventario.use-potion-description', {
+            level: a.level,
+            regeneration: item.data.baseBoost + item.data.perLevel * a.level,
+            amount: a.amount,
+          }),
+        );
+
+        if (selectMenu.options.length < 25)
+          selectMenu.addOptions({
+            label: ctx.locale(`items:${a.id as 1}.name`),
+            value: `${a.id} | ${a.level} | ${a.amount}`,
+          });
+      });
+
+      const [itemsCustomId, newBase] = makeCustomId('USE');
+
+      selectMenu
+        .setMaxValues(selectMenu.options.length > 5 ? 5 : selectMenu.options.length)
+        .setCustomId(itemsCustomId);
+
+      const exitButton = makeCloseCommandButton(newBase, ctx.i18n);
+
+      ctx.makeMessage({
+        embeds: [embed],
+        components: [actionRow([selectMenu]), actionRow([exitButton])],
+      });
+
+      const potionsToUse =
+        await Util.collectComponentInteractionWithStartingId<SelectMenuInteraction>(
+          ctx.channel,
+          ctx.author.id,
+          newBase,
+          15_000,
+          false,
+        );
+
+      if (!potionsToUse) {
+        ctx.makeMessage({
+          components: [actionRow(disableComponents(ctx.locale('common:timesup'), [selectMenu]))],
+        });
+        return;
+      }
+
+      if (resolveCustomId(potionsToUse.customId) === 'CLOSE_COMMAND') {
+        ctx.deleteReply();
+        return;
+      }
+
+      const [modalId, modalBaseId] = makeCustomId('MODAL');
+
+      const modal = new Modal()
+        .setCustomId(modalId)
+        .setTitle(ctx.locale('commands:inventario.use-potion-modal-title'));
+
+      potionsToUse.values.forEach((a) => {
+        const [potionId, , maxItems] = resolveSeparatedStrings(a);
+        const input = new TextInputComponent()
+          .setCustomId(a)
+          .setMinLength(1)
+          .setMaxLength(2)
+          .setPlaceholder('1')
+          .setValue('1')
+          .setStyle('SHORT')
+          .setRequired(true)
+          .setLabel(
+            ctx.locale('commands:inventario.use-potion-modal-lable', {
+              name: ctx.locale(`items:${potionId as '1'}.name`).substring(0, 39),
+              max: maxItems,
+            }),
+          );
+
+        modal.addComponents({ type: 1, components: [input] });
+      });
+
+      potionsToUse.showModal(modal).catch(debugError);
+
+      ctx.makeMessage({
+        components: [],
+        embeds: [],
+        content: ctx.prettyResponse('time', 'common:waiting-form'),
+      });
+
+      const modalResponse = await potionsToUse
+        .awaitModalSubmit({
+          time: 25_000,
+          filter: (int) => int.customId.startsWith(`${modalBaseId}`),
+        })
+        .catch(() => null);
+
+      if (!modalResponse) {
+        ctx.makeMessage({
+          embeds: [],
+          components: [],
+          content: ctx.prettyResponse('error', 'common:form-timesup'),
+        });
+        return;
+      }
+
+      modalResponse.deferUpdate();
+
+      let didError = false;
+
+      for (let i = 0; i < modalResponse.components.length; i++) {
+        const [itemId, itemLevel, maxAmount] = resolveSeparatedStrings(
+          modalResponse.components[i].components[0].customId,
+        );
+        const selectedAmount = parseInt(modalResponse.components[i].components[0].value);
+
+        if (
+          Number.isNaN(selectedAmount) ||
+          selectedAmount <= 0 ||
+          selectedAmount > Number(maxAmount)
+        ) {
+          didError = true;
+          break;
+        }
+
+        const item = getItemById<ConsumableItem>(Number(itemId));
+
+        for (let j = 0; j < Number(selectedAmount); i++) {
+          const toRegenValue = Math.floor(
+            item.data.baseBoost + item.data.perLevel * Number(itemLevel),
+          );
+          const toRegenType = item.data.boostType;
+
+          user[toRegenType] += toRegenValue;
+          removeFromInventory([{ id: Number(itemId), level: Number(itemLevel) }], user.inventory);
+        }
+      }
+
+      if (didError) {
+        ctx.makeMessage({
+          content: ctx.prettyResponse('error', 'commands:inventario.use-potion-invalid-selection'),
+        });
+        return;
+      }
+
+      if (user.life > getUserMaxLife(user)) user.life = getUserMaxLife(user);
+      if (user.mana > getUserMaxMana(user)) user.mana = getUserMaxMana(user);
+
+      await ctx.client.repositories.roleplayRepository.updateUser(ctx.author.id, {
+        life: user.life,
+        mana: user.mana,
+        inventory: user.inventory,
+      });
+
+      ctx.makeMessage({
+        embeds: [],
+        components: [],
+        content: ctx.prettyResponse('success', 'commands:inventario.use-potion-success'),
+      });
+      return;
+    }
 
     embed.setDescription('');
 
-    const availablePotions = resolvedItems.filter((a) => a.data.type === 'potion') as Array<
-      IReturnData<ConsumableItem> & { level: number; amount: number }
-    >;
+    const availableEquipments = resolvedItems.filter((a) =>
+      ['weapon', 'protection', 'backpack'].includes(a.data.type),
+    ) as Array<IReturnData<EquipmentItem> & { level: number; amount: number }>;
 
     const selectMenu = new MessageSelectMenu().setCustomId(`${ctx.interaction.id} | SELECT`);
 
-    const showedItems: { id: number; level: number }[] = [];
+    availableEquipments.forEach((a) => {
+      const item = getEquipmentById(a.id);
 
-    availablePotions.forEach((a) => {
-      if (!showedItems.some((b) => a.id === b.id && a.level === b.level))
-        showedItems.push({ id: a.id, level: a.level });
-      if (selectMenu.options.length < 25) {
-        for (let i = 0; i < a.amount; i++) {
-          selectMenu.addOptions({
-            label: ctx.locale(`items:${a.id as 1}.name`),
-            value: `${a.id} | ${a.level} | ${i}`,
-          });
-        }
+      const cannotEquip =
+        item.data.type === 'backpack' &&
+        item.data.levels[a.level].value < user.inventory.reduce((p, c) => p + c.amount, 0);
+
+      embed.addField(
+        ctx.locale(`items:${a.id as 1}.name`),
+        ctx.locale('commands:inventario.use-equipment-description', {
+          level: a.level,
+          field: ctx.locale(`commands:inventario.fields.${item.data.type}`),
+          value: item.data.levels[a.level].value,
+          backpackFull: cannotEquip ? ctx.locale('commands:inventario.backpack-full') : '',
+        }),
+      );
+
+      if (selectMenu.options.length < 25 && !cannotEquip) {
+        selectMenu.addOptions({
+          label: ctx.locale(`items:${a.id as 1}.name`),
+          value: `${a.id} | ${a.level}`,
+        });
       }
     });
 
-    showedItems.forEach((a) => {
-      const item = getItemById<ConsumableItem>(a.id);
-      embed.addField(
-        ctx.locale(`items:${a.id as 1}.name`),
-        ctx.locale('commands:inventario.use-description', {
-          level: a.level,
-          regeneration: item.data.baseBoost + item.data.perLevel * a.level,
-        }),
-      );
-    });
+    const [itemsCustomId, newBase] = makeCustomId('EQUIP');
 
-    const [itemsCustomId, newBase] = makeCustomId('USE');
-
-    selectMenu.setMaxValues(selectMenu.options.length).setCustomId(itemsCustomId);
+    selectMenu.setMaxValues(1).setCustomId(itemsCustomId);
 
     const exitButton = makeCloseCommandButton(newBase, ctx.i18n);
 
     ctx.makeMessage({
       embeds: [embed],
-      components: [actionRow([exitButton]), actionRow([selectMenu])],
+      components: [actionRow([selectMenu]), actionRow([exitButton])],
     });
 
-    const selectedItems =
+    const selectedEquipment =
       await Util.collectComponentInteractionWithStartingId<SelectMenuInteraction>(
         ctx.channel,
         ctx.author.id,
         newBase,
-        7000,
+        12_000,
       );
 
-    if (!selectedItems) {
+    if (!selectedEquipment) {
       ctx.makeMessage({
         components: [actionRow(disableComponents(ctx.locale('common:timesup'), [selectMenu]))],
       });
       return;
     }
 
-    if (resolveCustomId(selectedItems.customId) === 'CLOSE_COMMAND') {
+    if (resolveCustomId(selectedEquipment.customId) === 'CLOSE_COMMAND') {
       ctx.deleteReply();
       return;
     }
 
-    selectedItems.values.forEach((a) => {
-      const [itemId, itemLevel] = resolveSeparatedStrings(a);
-      const item = getItemById<ConsumableItem>(Number(itemId));
+    const [itemIdToEquip, itemLevelToEquip] = resolveSeparatedStrings(selectedEquipment.values[0]);
+    const item = getEquipmentById(Number(itemIdToEquip));
 
-      const toRegenValue = Math.floor(item.data.baseBoost + item.data.perLevel * Number(itemLevel));
-      const toRegenType = item.data.boostType;
+    const changedItem = user[item.data.type];
+    const toAddItem = { id: Number(itemIdToEquip), level: Number(itemLevelToEquip) };
 
-      user[toRegenType] += toRegenValue;
-      removeFromInventory([{ id: Number(itemId), level: Number(itemLevel) }], user.inventory);
-    });
+    removeFromInventory([toAddItem], user.inventory);
 
-    if (user.life > getUserMaxLife(user)) user.life = getUserMaxLife(user);
-    if (user.mana > getUserMaxMana(user)) user.mana = getUserMaxMana(user);
+    addToInventory([{ id: changedItem.id, level: changedItem.level }], user.inventory);
 
     await ctx.client.repositories.roleplayRepository.updateUser(ctx.author.id, {
-      life: user.life,
-      mana: user.mana,
+      [item.data.type]: toAddItem,
       inventory: user.inventory,
     });
 
     ctx.makeMessage({
       embeds: [],
       components: [],
-      content: ctx.prettyResponse('success', 'commands:inventario.use-success'),
+      content: ctx.prettyResponse('success', 'commands:inventario.equip-success', {
+        name: ctx.locale(`items:${itemIdToEquip as '1'}.name`),
+      }),
     });
   }
 }
