02f5637d0b5738af44a2bf2953f0a5f8690c6e65
Luxanna <CommandPlayermine@gmail.com>
2022-07-11 12:33:02 -0300

    feat: create poker repository

---
+++ src/database/repositories/PokerRepository.ts
@@ -0,0 +1,22 @@
+import { debugError, MayNotExists } from '@utils/Util';
+import { Redis } from 'ioredis';
+
+export default class PokerRepository {
+  constructor(private redisClient: MayNotExists<Redis>) {}
+
+  async isUserInPokerMatch(userId: string): Promise<boolean> {
+    if (!this.redisClient) return false;
+    const res = await this.redisClient.sismember('poker_match', userId);
+    return res === 1;
+  }
+
+  async addUserToPokerMatch(userId: string): Promise<void> {
+    if (!this.redisClient) return;
+    await this.redisClient.sadd('poker_match', userId).catch(debugError);
+  }
+
+  async removeUserFromPokerMatch(userId: string): Promise<void> {
+    if (!this.redisClient) return;
+    await this.redisClient.srem('poker_match', userId).catch(debugError);
+  }
+}
