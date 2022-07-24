5abc53560168330192ff41b45f2636bfe7361a9b
Luxanna <CommandPlayermine@gmail.com>
2022-07-24 01:20:09 -0300

    refactor: rewrite slap command

---
+++ packages/events/src/commands/actions/SlapCommand.ts
@@ -0,0 +1,79 @@
+import { User } from 'discordeno/transformers';
+import { ApplicationCommandOptionTypes } from 'discordeno/types';
+
+import { createCommand } from '../../structures/command/createCommand';
+import { MessageFlags } from '../../utils/discord/messageUtils';
+import { TODAYS_YEAR, COLORS } from '../../structures/constants';
+import { getAssetLink } from '../../structures/cdnManager';
+import { getUserAvatar, mentionUser } from '../../utils/discord/userUtils';
+import { createEmbed } from '../../utils/discord/createEmbed';
+import { capitalize } from '../../utils/stringUtils';
+
+const SlapCommand = createCommand({
+  path: '',
+  name: 'tapa',
+  nameLocalizations: { 'en-US': 'slap' },
+  description: '「🖐️」・Da um tapão de arrancar tumor em alguém',
+  descriptionLocalizations: { 'en-US': '「🖐️」・Give someone a tumor-pulling slap' },
+  options: [
+    {
+      name: 'user',
+      type: ApplicationCommandOptionTypes.User,
+      description: 'Usuário pra mete o tapa',
+      descriptionLocalizations: { 'en-US': 'User to slap' },
+      required: true,
+    },
+    {
+      name: 'motivo',
+      nameLocalizations: { 'en-US': 'reason' },
+      type: ApplicationCommandOptionTypes.String,
+      description: 'Por que tu quer fazer isso?',
+      descriptionLocalizations: { 'en-US': 'Why do you wanna do that?' },
+      required: false,
+    },
+  ],
+  category: 'actions',
+  authorDataFields: [],
+  execute: async (ctx) => {
+    const user = ctx.getOption<User>('user', 'users', true);
+    const reason = ctx.getOption<string>('motivo', false);
+
+    if (user.toggles.bot) {
+      await ctx.makeMessage({
+        content: ctx.prettyResponse('error', 'commands:tapa.bot'),
+      });
+      return;
+    }
+
+    if (user.id === ctx.author.id) {
+      await ctx.makeMessage({
+        content: ctx.prettyResponse('error', 'commands:tapa.self-mention'),
+        flags: MessageFlags.EPHEMERAL,
+      });
+      return;
+    }
+
+    const avatar = getUserAvatar(ctx.author, { enableGif: true });
+    const selectedImage = getAssetLink('slap');
+
+    const embed = createEmbed({
+      title: ctx.locale('commands:tapa.embed_title'),
+      description: ctx.locale('commands:tapa.embed_description', {
+        author: mentionUser(ctx.author.id),
+        mention: mentionUser(user.id),
+      }),
+      image: { url: selectedImage },
+      color: COLORS.ACTIONS,
+      thumbnail: { url: avatar },
+    });
+
+    if (reason)
+      embed.description = `${embed.description}\n\n_"${capitalize(
+        reason,
+      )}"_ - ${ctx.author.username.toUpperCase()}, ${TODAYS_YEAR}`;
+
+    await ctx.makeMessage({ embeds: [embed] });
+  },
+});
+
+export default SlapCommand;
