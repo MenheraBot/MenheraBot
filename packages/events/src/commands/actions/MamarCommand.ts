ae7e28dca1945f8fef7897daae636661b628a510
Luxanna <CommandPlayermine@gmail.com>
2022-07-25 02:39:36 -0300

    refactor: rewrite mamar command

---
+++ packages/events/src/commands/actions/MamarCommand.ts
@@ -0,0 +1,84 @@
+import { User } from 'discordeno/transformers';
+import { ApplicationCommandOptionTypes } from 'discordeno/types';
+
+import blacklistRepository from '../../database/repositories/blacklistRepository';
+import relationshipRepostory from '../../database/repositories/relationshipRepostory';
+import { createCommand } from '../../structures/command/createCommand';
+import { MessageFlags } from '../../utils/discord/messageUtils';
+import { TODAYS_YEAR, COLORS } from '../../structures/constants';
+import { getAssetLink } from '../../structures/cdnManager';
+import { getUserAvatar, mentionUser } from '../../utils/discord/userUtils';
+import { createEmbed } from '../../utils/discord/createEmbed';
+import { capitalize } from '../../utils/stringUtils';
+
+const BicudaCommand = createCommand({
+  path: '',
+  name: 'mamar',
+  nameLocalizations: { 'en-US': 'lick' },
+  description: '「😝」・De uma mamada de Qualidade monstra em alguém',
+  descriptionLocalizations: { 'en-US': '「😝」・Lick someone' },
+  options: [
+    {
+      type: ApplicationCommandOptionTypes.User,
+      name: 'user',
+      description: 'Usuário que você quer mamar',
+      descriptionLocalizations: { 'en-US': 'User you want to lick' },
+      required: true,
+    },
+    {
+      type: ApplicationCommandOptionTypes.String,
+      name: 'motivo',
+      nameLocalizations: { 'en-US': 'reason' },
+      description: 'Por que tu quer mamar?',
+      descriptionLocalizations: { 'en-US': 'Why do you wanna lick?' },
+      required: false,
+    },
+  ],
+  category: 'actions',
+  authorDataFields: [],
+  execute: async (ctx) => {
+    const user = ctx.getOption<User>('user', 'users', true);
+    const reason = ctx.getOption<string>('motivo', false);
+
+    if (user.id === ctx.author.id) {
+      await ctx.makeMessage({
+        content: ctx.prettyResponse('error', 'commands:mamar.self-mention'),
+        flags: MessageFlags.EPHEMERAL,
+      });
+      return;
+    }
+
+    if (await blacklistRepository.isUserBanned(user.id)) {
+      await ctx.makeMessage({
+        content: ctx.prettyResponse('error', 'commands:mamar.user-banned'),
+        flags: MessageFlags.EPHEMERAL,
+      });
+      return;
+    }
+
+    const avatar = getUserAvatar(ctx.author, { enableGif: true });
+    const selectedImage = getAssetLink('mamar');
+
+    const embed = createEmbed({
+      title: ctx.locale('commands:mamar.embed_title'),
+      description: ctx.locale('commands:mamar.embed_description', {
+        author: mentionUser(ctx.author.id),
+        mention: mentionUser(user.id),
+      }),
+      image: { url: selectedImage },
+      color: COLORS.ACTIONS,
+      thumbnail: { url: avatar },
+    });
+
+    if (reason)
+      embed.description = `${embed.description}\n\n_"${capitalize(
+        reason,
+      )}"_ - ${ctx.author.username.toUpperCase()}, ${TODAYS_YEAR}`;
+
+    await ctx.makeMessage({ embeds: [embed] });
+
+    await relationshipRepostory.executeMamar(ctx.author.id, user.id);
+  },
+});
+
+export default BicudaCommand;
