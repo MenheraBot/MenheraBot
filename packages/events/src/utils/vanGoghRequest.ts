cc087ea0eeca0c10417f5cd2572a84242b3eda9c
Luxanna <CommandPlayermine@gmail.com>
2022-07-25 03:30:15 -0300

    feat: create vangogh request

---
+++ packages/events/src/utils/vanGoghRequest.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+import { getEnviroments } from './getEnviroments';
+
+const { VANGOGH_URL, MENHERA_AGENT, VANGOGH_TOKEN } = getEnviroments([
+  'VANGOGH_URL',
+  'MENHERA_AGENT',
+  'VANGOGH_TOKEN',
+]);
+
+const VanGoghApi = axios.create({
+  baseURL: VANGOGH_URL,
+  timeout: 5_000,
+  headers: {
+    'Content-Type': 'application/json',
+    'User-Agent': MENHERA_AGENT,
+    Authorization: VANGOGH_TOKEN,
+  },
+});
+
+enum VangoghRoutes {
+  Fluffety = 'fluffety',
+  Astolfo = 'astolfo',
+  Vasco = 'vasco',
+  Preview = 'preview',
+  EightBall = '8ball',
+  Philo = 'philo',
+  Ship = 'ship',
+  Trisal = 'trisal',
+  Profile = 'profile',
+  Gado = 'gado',
+  Macetava = 'macetava',
+  Blackjack = 'blackjack',
+  BETA = 'beta',
+}
+
+interface ErrorReturn {
+  err: true;
+}
+
+interface SuccessReturn {
+  err?: false;
+  data: Buffer;
+}
+
+export type VanGoghReturnData = ErrorReturn | SuccessReturn;
+
+const vanGoghRequest = async <T>(route: VangoghRoutes, data: T): Promise<VanGoghReturnData> => {
+  const result = await VanGoghApi.post(`/${route}`, data).catch(() => null);
+  if (!result) return { err: true };
+  return { err: false, data: Buffer.from(result.data, 'base64') };
+};
+
+export { vanGoghRequest };
