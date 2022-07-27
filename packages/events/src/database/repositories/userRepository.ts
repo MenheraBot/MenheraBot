92a27d77c4535205ca5317056b129adb8c3fe9b3
ySnoopyDogy <CommandPlayermine@gmail.com>
2022-07-26 23:14:00 -0300

    feat: create new functions to user repository

--- packages/events/src/database/repositories/userRepository.ts
+++ packages/events/src/database/repositories/userRepository.ts
@@ -2,6 +2,40 @@ import { UpdateQuery } from 'mongoose';
 
 import { usersModel } from '../collections';
 import { DatabaseUserSchema } from '../../types/database';
+import { RedisClient } from '../databases';
+
+const parseMongoUserToRedisUser = (user: DatabaseUserSchema): DatabaseUserSchema => ({
+  angels: user.angels,
+  ban: user.ban,
+  banReason: user.banReason,
+  lastCommandAt: user.lastCommandAt,
+  archangels: user.archangels,
+  badges: user.badges,
+  id: user.id,
+  colors: user.colors,
+  demigods: user.demigods,
+  demons: user.demons,
+  estrelinhas: user.estrelinhas,
+  giants: user.giants,
+  gods: user.gods,
+  hiddingBadges: user.hiddingBadges,
+  huntCooldown: user.huntCooldown,
+  info: user.info,
+  inUseItems: user.inUseItems,
+  inventory: user.inventory,
+  isBot: user.isBot,
+  itemsLimit: user.itemsLimit,
+  mamado: user.mamado,
+  mamou: user.mamou,
+  married: user.married,
+  marriedAt: user.marriedAt,
+  marriedDate: user.marriedDate,
+  rolls: user.rolls,
+  selectedColor: user.selectedColor,
+  trisal: user.trisal,
+  voteCooldown: user.voteCooldown,
+  votes: user.votes,
+});
 
 const updateUser = async (
   userId: bigint,
@@ -10,4 +44,39 @@ const updateUser = async (
   await usersModel.updateOne({ id: userId }, { ...query, lastCommandAt: Date.now() });
 };
 
-export default { updateUser };
+const findOrCreate = async (
+  userId: bigint,
+  projection: Array<keyof DatabaseUserSchema> = [],
+): Promise<DatabaseUserSchema> => {
+  const fromRedis = await RedisClient.get(`user:${userId}`);
+
+  if (fromRedis) return JSON.parse(fromRedis);
+
+  const fromMongo = await usersModel.findOne({ id: userId }, projection);
+
+  if (!fromMongo) {
+    const newUser = await usersModel.create({ id: userId });
+
+    await RedisClient.setex(
+      `user:${userId}`,
+      3600,
+      JSON.stringify(parseMongoUserToRedisUser(newUser)),
+    );
+
+    return newUser;
+  }
+
+  await RedisClient.setex(
+    `user:${userId}`,
+    3600,
+    JSON.stringify(parseMongoUserToRedisUser(fromMongo)),
+  );
+
+  return fromMongo;
+};
+
+const getBannedUserInfo = async (userId: bigint): Promise<DatabaseUserSchema | null> => {
+  return usersModel.findOne({ id: userId }, ['ban', 'banReason'], { lean: true });
+};
+
+export default { updateUser, getBannedUserInfo, findOrCreate };
