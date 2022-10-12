c55d14b006469d0553ca6a46979a9b0012b12f6b
ySnoopyDogy <CommandPlayermine@gmail.com>
2022-10-12 04:02:40 -0300

    feat: create return type from utils functions

--- packages/events/src/types/menhera.d.ts
+++ packages/events/src/types/menhera.d.ts
@@ -1,6 +1,11 @@
 import { Bot, Collection } from 'discordeno';
 import { ChatInputInteractionCommand } from './commands';
 
+export interface IdentifiedData<T> {
+  id: number;
+  data: T;
+}
+
 export interface MenheraClient extends Bot {
   commands: Collection<string, ChatInputInteractionCommand>;
   ownerId: bigint;
