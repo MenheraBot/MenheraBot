9860bf3af15a00b280276613b74b64380354b72c
ySnoopyDogy <CommandPlayermine@gmail.com>
2022-11-08 01:19:51 -0300

    test: 100% coverage for angry command

--- packages/events/src/commands/actions/__tests__/AngryCommand.test.ts
+++ packages/events/src/commands/actions/__tests__/AngryCommand.test.ts
@@ -1,4 +1,5 @@
 import { jest } from '@jest/globals';
+import i18next from 'i18next';
 import { closeConnections } from '../../../database/databases';
 import angryCommand from '../AngryCommand';
 
@@ -13,13 +14,18 @@ describe('AngryCommand tests', () => {
     prettyResponse: (_: unknown, text: string) => text,
     author: {
       discriminator: 1111,
+      id: 1,
+      username: 'veigar',
     },
-    locale: (text: string) => text,
+    locale: jest.fn((text: string, data: Record<string, string> = {}) =>
+      i18next.getFixedT('pt-BR')(text, data),
+    ),
   };
 
   beforeEach(() => {
     context.getOption = jest.fn();
     context.makeMessage.mockClear();
+    context.locale.mockClear();
   });
 
   it('should return a message saying that cannot mention bots', () => {
@@ -35,10 +41,43 @@ describe('AngryCommand tests', () => {
     // @ts-expect-error Mocking UwU
     angryCommand.execute(context);
 
-    // @ts-expect-error Didnt find a best way of doing it
-    expect(context.makeMessage.mock.calls[0][0].embeds[0].title).toBe(
-      'commands:raiva.no-mention.embed_title',
-    );
+    expect(context.locale.mock.calls[0][0]).toBe('commands:raiva.no-mention.embed_title');
+  });
+
+  it('should has a reason when talking about the author only', () => {
+    context.getOption.mockReturnValueOnce(undefined);
+    context.getOption.mockReturnValueOnce('motivo tri');
+    // @ts-expect-error Mocking UwU
+    angryCommand.execute(context);
+
+    expect(
+      // @ts-expect-error Didnt find a best way of doing it
+      context.makeMessage.mock.calls[0][0].embeds[0].description.includes('Motivo tri'),
+    ).toBe(true);
+  });
+
+  it('should send a embed mentioning the author and the target', () => {
+    context.getOption.mockReturnValueOnce({ id: 2 });
+
+    // @ts-expect-error Mocking UwU
+    angryCommand.execute(context);
+
+    expect(
+      // @ts-expect-error Didnt find a best way of doing it
+      [1, 2].every((id) => context.makeMessage.mock.calls[0][0].embeds[0].description.includes(id)),
+    ).toBe(true);
+  });
+
+  it('should include the reason in the final embed if given', () => {
+    context.getOption.mockReturnValueOnce({ id: 2 });
+    context.getOption.mockReturnValueOnce('motivo tri');
+    // @ts-expect-error Mocking UwU
+    angryCommand.execute(context);
+
+    expect(
+      // @ts-expect-error Didnt find a best way of doing it
+      context.makeMessage.mock.calls[0][0].embeds[0].description.includes('Motivo tri'),
+    ).toBe(true);
   });
 });
 
