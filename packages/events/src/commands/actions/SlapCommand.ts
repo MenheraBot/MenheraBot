784dcf881e635daa1a908b9d0d66c9b5d2182dfb
ySnoopyDogy <CommandPlayermine@gmail.com>
2022-11-10 20:05:45 -0300

    feat: add resolve logic to slap command

--- packages/events/src/commands/actions/SlapCommand.ts
+++ packages/events/src/commands/actions/SlapCommand.ts
@@ -34,24 +34,24 @@ const SlapCommand = createCommand({
   ],
   category: 'actions',
   authorDataFields: [],
-  execute: async (ctx) => {
+  execute: async (ctx, finishCommand) => {
     const user = ctx.getOption<User>('user', 'users', true);
     const reason = ctx.getOption<string>('motivo', false);
 
-    if (user.toggles.bot) {
-      await ctx.makeMessage({
-        content: ctx.prettyResponse('error', 'commands:tapa.bot'),
-      });
-      return;
-    }
+    if (user.toggles.bot)
+      return finishCommand(
+        ctx.makeMessage({
+          content: ctx.prettyResponse('error', 'commands:tapa.bot'),
+        }),
+      );
 
-    if (user.id === ctx.author.id) {
-      await ctx.makeMessage({
-        content: ctx.prettyResponse('error', 'commands:tapa.self-mention'),
-        flags: MessageFlags.EPHEMERAL,
-      });
-      return;
-    }
+    if (user.id === ctx.author.id)
+      return finishCommand(
+        ctx.makeMessage({
+          content: ctx.prettyResponse('error', 'commands:tapa.self-mention'),
+          flags: MessageFlags.EPHEMERAL,
+        }),
+      );
 
     const avatar = getUserAvatar(ctx.author, { enableGif: true });
     const selectedImage = getAssetLink('slap');
@@ -73,6 +73,7 @@ const SlapCommand = createCommand({
       )}"_ - ${ctx.author.username.toUpperCase()}, ${TODAYS_YEAR}`;
 
     await ctx.makeMessage({ embeds: [embed] });
+    finishCommand();
   },
 });
 
