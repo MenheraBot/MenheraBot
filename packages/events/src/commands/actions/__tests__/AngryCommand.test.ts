e3f4d3d6cb889d15bf0e5c2caa9edc8357c35bab
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-04-15 12:46:15 -0300

    ci: force a test fail to test CI

--- packages/events/src/commands/actions/__tests__/AngryCommand.test.ts
+++ packages/events/src/commands/actions/__tests__/AngryCommand.test.ts
@@ -76,6 +76,10 @@ describe('AngryCommand tests', () => {
       context.makeMessage.mock.calls[0][0].embeds[0].description.includes('Motivo tri'),
     ).toBe(true);
   });
+
+  it('should fail tests', () => {
+    expect(1).toBe(2);
+  });
 });
 
 export {};
