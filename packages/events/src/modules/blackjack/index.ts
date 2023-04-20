45869da1fcb6260134a57ed43e6f8b1cb2b730fb
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-04-19 22:20:55 -0300

    fix: blackjack bug

--- packages/events/src/modules/blackjack/index.ts
+++ packages/events/src/modules/blackjack/index.ts
@@ -1,9 +1,9 @@
 const BLACKJACK_CARDS = Array.from({ length: 52 }, (_, i) => i + 1);
 
 const BLACKJACK_PRIZE_MULTIPLIERS = {
-  init_blackjack: 1.5,
-  blackjack: 1,
-  base: 1,
+  init_blackjack: 2.5,
+  blackjack: 2,
+  base: 2,
 };
 
 const shuffleCards = (): number[] => {
