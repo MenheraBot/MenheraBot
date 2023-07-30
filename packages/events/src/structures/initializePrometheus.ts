7f7f0af0afec0de6551f4dad3b08ae4858679f52
ySnoopyDogy <git@luancaliel.dev>
2023-07-30 01:29:08 +0000

    chore: remove experimental prometheus counter

--- packages/events/src/structures/initializePrometheus.ts
+++ packages/events/src/structures/initializePrometheus.ts
@@ -3,7 +3,6 @@ import client, { Counter, Registry } from 'prom-client';
 let register: Registry;
 let commandsCounter: Counter;
 let interactionsCounter: Counter;
-let experimentsCommandsCounter: Counter;
 
 const initializePrometheus = (): void => {
   if (process.env.NOMICROSERVICES) return;
@@ -26,26 +25,12 @@ const initializePrometheus = (): void => {
     labelNames: ['type'],
   });
 
-  experimentsCommandsCounter = new client.Counter({
-    name: 'total_commands',
-    help: 'Amount off all comands with infos executed',
-    labelNames: ['user_id', 'guild_id', 'command_name'],
-  });
-
   register.registerMetric(commandsCounter);
   register.registerMetric(interactionsCounter);
-  register.registerMetric(experimentsCommandsCounter);
 };
 
 const getRegister = (): Registry => register;
 const getCommandsCounter = (): Counter => commandsCounter;
 const getInteractionsCounter = (): Counter => interactionsCounter;
-const getExperimentsCommandsCounter = (): Counter => experimentsCommandsCounter;
 
-export {
-  initializePrometheus,
-  getRegister,
-  getExperimentsCommandsCounter,
-  getCommandsCounter,
-  getInteractionsCounter,
-};
+export { initializePrometheus, getRegister, getCommandsCounter, getInteractionsCounter };
