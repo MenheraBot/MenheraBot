3ab13d0e00d836b64f226ba38020b1d9178d83a7
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-10-05 20:36:40 -0300

    feat: add log in debug error

--- packages/events/src/utils/debugError.ts
+++ packages/events/src/utils/debugError.ts
@@ -3,7 +3,14 @@ import * as Sentry from '@sentry/node';
 import { logger } from './logger';
 
 export const debugError = (err: Error, toSentry = true): null => {
-  logger.error(err.message);
-  if (toSentry) Sentry.captureException(err);
+  logger.error('Debug Error', err.message);
+  if (toSentry)
+    try {
+      Sentry.captureException(err);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      logger.error('Error in debug error', e?.message ?? e);
+    }
+
   return null;
 };
