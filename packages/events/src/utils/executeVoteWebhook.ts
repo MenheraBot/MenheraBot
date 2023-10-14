ffb03bb2fc4b7b47fdbc0c3c0bde1bd5fd77276d
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-10-14 14:08:15 -0300

    feat: start new event

--- packages/events/src/utils/executeVoteWebhook.ts
+++ packages/events/src/utils/executeVoteWebhook.ts
@@ -62,7 +62,8 @@ const executeVoteWebhook = async (userId: string, isWeekend: boolean): Promise<v
 
   const userDM = await bot.helpers.getDmChannel(userId).catch(debugError);
 
-  if (userDM) bot.helpers.sendMessage(userDM.id, { embeds: [embed] }).catch(debugError);
+  if (userDM)
+    bot.helpers.sendMessage(userDM.id, { embeds: [embed] }).catch((e) => debugError(e, false));
 
   const updateData: UpdateQuery<DatabaseUserSchema> = {
     $inc: {
