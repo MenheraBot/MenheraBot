a6f5a0117a0b92a1604208d4c57a923c61100c46
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-11-15 18:45:47 -0300

    feat: hash over string redis optimization

--- packages/events/src/database/repositories/guildRepository.ts
+++ packages/events/src/database/repositories/guildRepository.ts
@@ -3,19 +3,20 @@ import { guildsModel } from '../collections';
 import { MainRedisClient } from '../databases';
 
 const updateGuildLanguage = async (guildId: BigString, language: string): Promise<void> => {
-  await MainRedisClient.setex(`language:${guildId}`, 3600, language);
+  MainRedisClient.hset(`guild:${guildId}`, 'language', language);
+
   await guildsModel.updateOne({ id: `${guildId}` }, { lang: language });
 };
 
 const getGuildLanguage = async (guildId: BigString): Promise<string> => {
-  const fromRedis = await MainRedisClient.get(`language:${guildId}`);
+  const fromRedis = await MainRedisClient.hget(`guild:${guildId}`, 'language');
 
   if (fromRedis) return fromRedis;
 
   const fromMongo = await guildsModel.findOne({ id: `${guildId}` });
 
   if (fromMongo) {
-    await MainRedisClient.setex(`language:${guildId}`, 3600, fromMongo.lang);
+    await MainRedisClient.hset(`guild:${guildId}`, 'language', fromMongo.lang);
 
     return fromMongo.lang;
   }
