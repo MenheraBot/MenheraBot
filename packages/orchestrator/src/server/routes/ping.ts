0b7af6a1e57ea4332c082443f902cde5dd898e1a
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-11-22 01:34:57 -0300

    feat: allow head on ping

--- packages/orchestrator/src/server/routes/ping.ts
+++ packages/orchestrator/src/server/routes/ping.ts
@@ -1,23 +1,32 @@
 import Router from 'koa-router';
 import { HTTPResponseCodes } from 'discordeno/types';
+import { Context } from 'koa';
 import { RequestType, sendEvent } from '../..';
+
+const handleRequest = async (ctx: Context): Promise<void> => {
+  ctx.status = HTTPResponseCodes.Ok;
+
+  if (ctx.req.method === 'HEAD') {
+    ctx.body = null;
+    ctx.status = 200;
+    ctx.set('Content-Type', 'application/json');
+    return;
+  }
+
+  const pings = await sendEvent(RequestType.AreYouOk, Date.now());
+
+  ctx.body = {
+    orchestrator: {
+      uptime: Math.floor(process.uptime() * 1000),
+    },
+    events: pings ?? [],
+  };
+};
 
 const createPingRouter = (): Router => {
   const router = new Router();
-
-  router.get('/ping', async (ctx) => {
-    ctx.status = HTTPResponseCodes.Ok;
-
-    const pings = await sendEvent(RequestType.AreYouOk, Date.now());
-
-    ctx.body = {
-      orchestrator: {
-        uptime: Math.floor(process.uptime() * 1000),
-      },
-      events: pings ?? [],
-    };
-  });
-
+  router.head('/ping', handleRequest);
+  router.get('/ping', handleRequest);
   return router;
 };
 
