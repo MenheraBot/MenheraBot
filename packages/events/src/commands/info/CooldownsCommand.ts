9da11ab1b237b30f44f67755c10f7154e7336c88
ySnoopyDogy <CommandPlayermine@gmail.com>
2024-04-21 20:17:13 -0300

    feat: display plantation states in cooldowns
    feat: add a way to see other users cooldowns
    
    Suggested-by: 585639256949522461

--- packages/events/src/commands/info/CooldownsCommand.ts
+++ packages/events/src/commands/info/CooldownsCommand.ts
@@ -1,43 +1,96 @@
-import { ActionRow, ButtonStyles, DiscordEmbedField } from 'discordeno/types';
+import {
+  ActionRow,
+  ApplicationCommandOptionTypes,
+  ButtonStyles,
+  DiscordEmbedField,
+} from 'discordeno/types';
 
+import { User } from 'discordeno/transformers';
 import { bot } from '../../index';
 import { createActionRow, createButton } from '../../utils/discord/componentUtils';
 import { createEmbed, hexStringToNumber } from '../../utils/discord/embedUtils';
 import { createCommand } from '../../structures/command/createCommand';
+import farmerRepository from '../../database/repositories/farmerRepository';
+import userRepository from '../../database/repositories/userRepository';
+import { MessageFlags } from '../../utils/discord/messageUtils';
+import { getDisplayName } from '../../utils/discord/userUtils';
+import { InteractionContext } from '../../types/menhera';
+import { getPlantationState } from '../../modules/fazendinha/plantationState';
+import { millisToSeconds } from '../../utils/miscUtils';
+
+const canDo = (value: number): boolean => value <= 0;
+
+const createField = (
+  ctx: InteractionContext,
+  type: string,
+  cooldown: number,
+  username: string,
+): DiscordEmbedField => ({
+  name: ctx.locale(`commands:cooldowns.${type as 'vote'}`),
+  value: ctx.locale(
+    canDo(cooldown) ? 'commands:cooldowns.no-cooldown' : 'commands:cooldowns.time',
+    {
+      unix: millisToSeconds(cooldown + Date.now()),
+      user: username,
+    },
+  ),
+  inline: false,
+});
 
 const CooldownsCommand = createCommand({
   path: '',
   name: 'cooldowns',
-  description: '「⌛」・Mostra todos os seus tempos de recarga',
-  descriptionLocalizations: { 'en-US': '「⌛」・Shows all your cooldowns' },
+  description: '「⌛」・Mostra os principais recursos de um usuário',
+  descriptionLocalizations: { 'en-US': '「⌛」・Shows the main resources from an user' },
   category: 'info',
+  options: [
+    {
+      name: 'user',
+      type: ApplicationCommandOptionTypes.User,
+      description: 'Usuário para ver os recursos',
+      descriptionLocalizations: { 'en-US': 'User to see the main resources' },
+      required: false,
+    },
+  ],
   authorDataFields: ['huntCooldown', 'voteCooldown', 'selectedColor'],
   execute: async (ctx, finishCommand) => {
-    const canDo = (value: number): boolean => value <= 0;
+    const userToUse = ctx.getOption<User>('user', 'users', false) ?? ctx.user;
+    const userData =
+      userToUse.id === ctx.user.id ? ctx.authorData : await userRepository.findUser(userToUse.id);
 
-    const createField = (type: string, cooldown: number): DiscordEmbedField => ({
-      name: ctx.locale(`commands:cooldowns.${type as 'vote'}`),
-      value: ctx.locale(
-        canDo(cooldown) ? 'commands:cooldowns.no-cooldown' : 'commands:cooldowns.time',
-        {
-          unix: Math.floor((cooldown + Date.now()) / 1000),
-        },
-      ),
-      inline: false,
-    });
+    if (!userData)
+      return ctx.makeMessage({
+        content: ctx.prettyResponse('error', 'commands:cooldowns.no-user'),
+        flags: MessageFlags.EPHEMERAL,
+      });
 
-    const huntCooldown = ctx.authorData.huntCooldown - Date.now();
-    const voteCooldown = ctx.authorData.voteCooldown - Date.now();
+    const huntCooldown = userData.huntCooldown - Date.now();
+    const voteCooldown = userData.voteCooldown - Date.now();
+    const displayName = getDisplayName(userToUse);
+
+    const farmer = await farmerRepository.getFarmer(userToUse.id);
+
+    const farmerFields = farmer.plantations.map((p, i) => ({
+      name: ctx.locale('commands:cooldowns.field', { field: i + 1 }),
+      value: ctx.locale(`commands:cooldowns.field-states.${getPlantationState(p)[0]}`, {
+        unix: millisToSeconds(getPlantationState(p)[1]),
+      }),
+      inline: true,
+    }));
 
     const embed = createEmbed({
-      title: ctx.locale('commands:cooldowns.title'),
-      color: hexStringToNumber(ctx.authorData.selectedColor),
-      fields: [createField('vote', voteCooldown), createField('hunt', huntCooldown)],
+      title: ctx.locale('commands:cooldowns.title', { user: getDisplayName(userToUse) }),
+      color: hexStringToNumber(userData.selectedColor),
+      fields: [
+        createField(ctx, 'vote', voteCooldown, displayName),
+        createField(ctx, 'hunt', huntCooldown, displayName),
+        ...farmerFields,
+      ],
     });
 
     const components: ActionRow[] = [];
 
-    if (voteCooldown < 0)
+    if (voteCooldown < 0 && ctx.user.id === userToUse.id)
       components.push(
         createActionRow([
           createButton({
