f2c80b806cfb4b3acad072a734c0fe656e52ed03
ySnoopyDogy <CommandPlayermine@gmail.com>
2024-06-06 19:19:13 -0300

    fix: fair pagination

--- packages/events/src/database/repositories/fairRepository.ts
+++ packages/events/src/database/repositories/fairRepository.ts
@@ -35,10 +35,13 @@ const deleteAnnouncement = async (id: string): Promise<void> => {
 };
 
 const getAnnouncementIds = async (skip: number, take: number): Promise<string[]> => {
-  const [, result] = await MainRedisClient.sscan(`fair_announcement:all`, skip, 'COUNT', take);
+  const fullAnnouncements = await MainRedisClient.smembers(`fair_announcement:all`);
 
-  return result;
+  return fullAnnouncements.slice(skip, skip + take);
 };
+
+const getTotalAnnouncements = async (): Promise<number> =>
+  MainRedisClient.scard('fair_announcement:all');
 
 const getAnnoucementNames = async (language: AvailableLanguages): Promise<string[]> =>
   MainRedisClient.smembers(`fair_announcement:${language}`);
@@ -124,5 +127,6 @@ export default {
   constructAnnouncements,
   getAnnoucementNames,
   getAnnouncementIds,
+  getTotalAnnouncements,
   getAnnouncement,
 };
--- packages/events/src/modules/fazendinha/exploreFair.ts
+++ packages/events/src/modules/fazendinha/exploreFair.ts
@@ -150,18 +150,23 @@ const displayFair = async (
   embedColor: string,
   user?: User,
 ) => {
-  const annonucements = user
+  const totalAnnouncements = await fairRepository.getTotalAnnouncements();
+
+  const itemsToPick =
+    page === 0 ? MAX_ITEMS_PER_FAIR_PAGE : totalAnnouncements - page * MAX_ITEMS_PER_FAIR_PAGE;
+
+  const announcements = user
     ? await fairRepository.getUserProducts(user.id)
     : await Promise.all(
         (
           await fairRepository.getAnnouncementIds(
             page * MAX_ITEMS_PER_FAIR_PAGE,
-            MAX_ITEMS_PER_FAIR_PAGE,
+            itemsToPick > MAX_ITEMS_PER_FAIR_PAGE ? MAX_ITEMS_PER_FAIR_PAGE : itemsToPick,
           )
         ).map((a) => fairRepository.getAnnouncement(a)),
       );
 
-  if (annonucements.length === 0)
+  if (announcements.length === 0)
     return ctx.makeMessage({
       embeds: [],
       components: [],
@@ -189,7 +194,7 @@ const displayFair = async (
     placeholder: ctx.locale('commands:fazendinha.feira.comprar.select-item'),
   });
 
-  annonucements.forEach((item, i) => {
+  announcements.forEach((item, i) => {
     if (!item) return;
 
     embed.description += `${ctx.locale('commands:fazendinha.feira.comprar.description', {
@@ -244,7 +249,7 @@ const displayFair = async (
       ),
       label: ctx.locale('common:next'),
       style: ButtonStyles.Primary,
-      disabled: selectMenu.options.length < MAX_ITEMS_PER_FAIR_PAGE,
+      disabled: page * MAX_ITEMS_PER_FAIR_PAGE + announcements.length >= totalAnnouncements,
     });
 
     componentsToSend.push(createActionRow([backButton, nextButton]));
