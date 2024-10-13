3baae4211122f58cddef0340d3c570c20d88943d
ySnoopyDogy <CommandPlayermine@gmail.com>
2024-10-13 15:28:49 -0300

    chore: advance in event shop development

--- packages/events/src/commands/fun/DiaDeMuertosCommand.ts
+++ packages/events/src/commands/fun/DiaDeMuertosCommand.ts
@@ -4,6 +4,7 @@ import { createCommand } from '../../structures/command/createCommand';
 import eventRepository from '../../database/repositories/eventRepository';
 import { createEmbed, hexStringToNumber } from '../../utils/discord/embedUtils';
 import { CempasuchilPlant, Plants } from '../../modules/fazendinha/constants';
+import { createCustomId, createSelectMenu } from '../../utils/discord/componentUtils';
 
 const buyableItems = [
   {
@@ -55,24 +56,41 @@ const DiaDeMuertosCommand = createCommand({
 
     const userEvents = await eventRepository.getUser(ctx.user.id);
 
+    const selectMenu = createSelectMenu({
+      customId: createCustomId(0, ctx.user.id, ctx.originalInteractionId, 'BUY'),
+      options: [],
+      minValues: 1,
+      maxValues: 1,
+      placeholder: ctx.locale('events:dia-dos-mortos.select-one'),
+    });
+
     const embed = createEmbed({
       title: ctx.prettyResponse('cempasuchil', 'events:dia-dos-mortos.embed-title'),
       description: ctx.locale('events:dia-dos-mortos.embed-description', {
         currency: ctx.locale('events:dia-dos-mortos.currency', { currency: userEvents.currency }),
       }),
       color: hexStringToNumber(ctx.authorData.selectedColor),
-      fields: buyableItems.map((item) => ({
-        name: ctx.locale(`events:dia-dos-mortos.prizes.${item.id}`),
-        value: ctx.locale(`events:dia-dos-mortos.prizes.description`, {
-          price: item.price,
-          emoji: Plants[CempasuchilPlant].emoji,
-        }),
-        inline: true,
-      })),
+      fields: buyableItems.map((item) => {
+        // TODO: Check if is oneTime and user already got it.
+        selectMenu.options.push({
+          label: ctx.locale(`events:dia-dos-mortos.prizes.${item.id}`),
+          value: item.id,
+        });
+
+        return {
+          name: ctx.locale(`events:dia-dos-mortos.prizes.${item.id}`),
+          value: ctx.locale(`events:dia-dos-mortos.prizes.description`, {
+            price: item.price,
+            emoji: Plants[CempasuchilPlant].emoji,
+          }),
+          inline: true,
+        };
+      }),
     });
 
     await ctx.makeMessage({
       embeds: [embed],
+      // TODO: If it has more than one option, send the select menu
     });
   },
 });
