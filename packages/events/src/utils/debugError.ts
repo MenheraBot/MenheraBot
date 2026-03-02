ca7089574a054273bff3a5454dfa809f04cd6711
Luan Caliel <CommandPlayermine@gmail.com>
2026-03-02 17:38:17 -0300

    chore: do not log 404

--- packages/events/src/utils/debugError.ts
+++ packages/events/src/utils/debugError.ts
@@ -3,12 +3,11 @@ import * as Sentry from '@sentry/node';
 import { logger } from './logger.js';
 
 export const debugError = (err: Error, toSentry = true): null => {
-  logger.error('Debug Error', err?.message);
-  logger.error(err);
-
   // @ts-expect-error Not every errors are http errors
   if (toSentry && err?.response?.status !== 404)
     try {
+      logger.error('Debug Error', err?.message);
+      logger.error(err);
       Sentry.captureException(err);
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (e: any) {
